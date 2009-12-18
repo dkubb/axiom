26f68b72762206fc3eac0101f1605e7ff00e4eb0
Dan Kubb <dan.kubb@gmail.com>
2009-12-18 11:23:28 -0800

    Refactored Relation spec

--- spec/unit/veritas/relation_spec.rb
+++ spec/unit/veritas/relation_spec.rb
@@ -6,6 +6,9 @@ describe Relation do
   before do
     @header = [ [ :id, Integer ] ]
     @body   = [ [ 1 ] ]
+
+    @relation = Relation.new(@header, @body)
+    @other    = Relation.new(@header, [ [ 2 ] ])
   end
 
   describe '.new' do
@@ -22,10 +25,7 @@ describe Relation do
 
   describe '#==' do
     describe 'with equivalent relations' do
-      before do
-        @relation = Relation.new(@header, @body)
-        @other    = Relation.new(@header, @body)
-      end
+      before { @other = Relation.new(@header, @body) }
 
       subject { @relation == @other }
 
@@ -33,11 +33,6 @@ describe Relation do
     end
 
     describe 'with different relations' do
-      before do
-        @relation = Relation.new(@header, @body)
-        @other    = Relation.new(@header, [ [ 2 ] ])
-      end
-
       subject { @relation == @other }
 
       it { should be_false }
@@ -45,11 +40,6 @@ describe Relation do
   end
 
   describe '#union' do
-    before do
-      @relation = Relation.new(@header, @body)
-      @other    = Relation.new(@header, [ [ 2 ] ])
-    end
-
     subject { @relation.union(@other) }
 
     it { should be_kind_of(Algebra::Union) }
