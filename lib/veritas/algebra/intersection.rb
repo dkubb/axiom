e119cf6b87a2d0a100780a0b1baf2737d503d28e
Dan Kubb <dan.kubb@gmail.com>
2009-12-18 20:58:56 -0800

    Added Algebra::Intersection#body

--- lib/veritas/algebra/intersection.rb
+++ lib/veritas/algebra/intersection.rb
@@ -2,6 +2,10 @@ module Veritas
   module Algebra
     class Intersection < Relation
       include SetOperation
+
+      def body
+        @body ||= @left.body & @right.body
+      end
     end # class Intersection
   end # module Algebra
 end # module Veritas
