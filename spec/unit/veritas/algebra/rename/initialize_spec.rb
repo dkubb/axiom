367290eb79fd1ae5c333940820fcd4bfb8fe6b85
Dan Kubb <dan.kubb@gmail.com>
2010-01-03 15:49:18 -0800

    Simplified Veritas::Algebra::Rename#initialize spec
    * If no argument is provided, then ruby will throw an exception.  No need
      to test whether or not ruby will handle this.  We should assume it does.

--- spec/unit/veritas/algebra/rename/initialize_spec.rb
+++ spec/unit/veritas/algebra/rename/initialize_spec.rb
@@ -3,24 +3,15 @@ require File.expand_path('../../../../../spec_helper', __FILE__)
 describe 'Veritas::Algebra::Rename#initialize' do
   before do
     @relation = Relation.new([ [ :id, Integer ] ], [ [ 1 ] ])
-    @aliases  = { :id => :other_id }
   end
 
-  describe 'with aliases' do
-    subject { Algebra::Rename.new(@relation, @aliases) }
+  subject { Algebra::Rename.new(@relation, :id => :other_id) }
 
-    it 'should set the relation' do
-      subject.relation.should equal(@relation)
-    end
-
-    it 'should set the header' do
-      subject.header.should == [ [ :other_id, Integer ] ]
-    end
+  it 'should set the relation' do
+    subject.relation.should equal(@relation)
   end
 
-  describe 'with no aliases' do
-    subject { Algebra::Rename.new(@relation) }
-
-    it { method(:subject).should raise_error(ArgumentError) }
+  it 'should set the header' do
+    subject.header.should == [ [ :other_id, Integer ] ]
   end
 end
