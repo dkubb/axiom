9551fd6cd76f6e4c6cba3213f0493fee7d8719e8
Dan Kubb <dan.kubb@gmail.com>
2010-01-03 14:56:40 -0800

    Added spec to ensure relations can be joined in any order

---
+++ spec/integration/veritas/algebra/join_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../../../../spec_helper', __FILE__)
+
+describe 'Veritas::Algebra::Join' do
+  describe 'Commutative property' do
+    before do
+      @left  = Relation.new([ [ :id, Integer ] ], [ [ 1 ], [ 2 ] ])
+      @right = Relation.new([ [ :id, Integer ], [ :name, String ] ], [ [ 1, 'Dan Kubb' ] ])
+    end
+
+    it 'should not matter which order the relations are joined in' do
+      @left.join(@right).should eql(@right.join(@left))
+    end
+  end
+end
