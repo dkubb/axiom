69bd36d0fef4257c5017af0cda7a7ad3323a3003
Dan Kubb <dan.kubb@gmail.com>
2010-01-08 16:11:54 -0800

    Added spec for Veritas::Relation::Operation::Order.new

---
+++ spec/unit/veritas/relation/operation/order/class_methods/new_spec.rb
@@ -0,0 +1,31 @@
+require File.expand_path('../../../../../../../spec_helper', __FILE__)
+
+describe 'Veritas::Relation::Operation::Order.new' do
+  before do
+    @relation = Relation.new([ [ :id, Integer ] ], [ [ 1 ], [ 2 ] ])
+  end
+
+  subject { Relation::Operation::Order.new(@relation, @directions) }
+
+  describe 'with all attributes specified in the directions' do
+    before do
+      @directions = [ @relation[:id] ]
+    end
+
+    it 'should set the relation' do
+      subject.relation.should equal(@relation)
+    end
+
+    it 'should set the directions' do
+      subject.directions.should == [ @relation[:id].asc ]
+    end
+  end
+
+  describe 'without all attributes specified in the directions' do
+    before do
+      @directions = []
+    end
+
+    it { method(:subject).should raise_error(ArgumentError, 'directions must include every attribute in the header') }
+  end
+end
