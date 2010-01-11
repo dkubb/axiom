4494609a5eb6cd56dfbd2625019ab20ba3519fe2
Dan Kubb <dan.kubb@gmail.com>
2010-01-10 19:08:34 -0800

    Updated Enumerable#take and #drop to match Ruby 1.8.7+ more closely
    * Added argument tests to #take and #drop based on Ruby 1.8.7+ behaviour
    * Simplified #take to not check Array#size every execution of #each and
      instead test a counter variable provided by #each_with_index
    * Simplified #drop to a single line, other than argument test
    * Removed reek warning for uncommunicative name

--- lib/veritas/core_ext/enumerable.rb
+++ lib/veritas/core_ext/enumerable.rb
@@ -1,20 +1,28 @@
 module Enumerable
 
   # Add Enumerable#take for Ruby 1.8.6
-  def take(n)
+  def take(limit)
+    assert_positive_size(limit, :take)
     array = []
-    each do |elem|
-      array << elem
-      break if array.size >= n
-    end unless n <= 0
+    each_with_index do |entry, index|
+      break if index == limit
+      array << entry
+    end
     array
   end unless method_defined? :take
 
   # Add Enumerable#drop for Ruby 1.8.6
-  def drop(n)
-    ary = to_a
-    return [] if n > ary.size
-    ary[n...ary.size]
+  def drop(offset)
+    assert_positive_size(offset, :drop)
+    to_a[offset..-1] || []
   end unless method_defined? :drop
+
+private
+
+  def assert_positive_size(number, method)
+    if number < 0
+      raise ArgumentError, "attempt to #{method} negative size"
+    end
+  end
 
 end
