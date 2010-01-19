b3b29e8d802a51685beb044e77a80087ec3e1c27
Dan Kubb <dan.kubb@gmail.com>
2010-01-18 23:16:28 -0800

    Simplified benchmarks
    * Benchmark 100 tuple relation options 5000x, which is probably on the
      higher side for relation size, but still not out of reach if doing
      things like joining relations from two different datastores.

--- benchmarks/speed.rb
+++ benchmarks/speed.rb
@@ -8,143 +8,91 @@ require 'rbench'
 require 'veritas'
 include Veritas
 
-TIMES  = 1
-COUNTS = [ 1, 10, 100, 1_000, 10_000, 100_000 ].freeze
-NAME   = 'Dan Kubb'.freeze
-HEADER = Relation::Header.new([ [ :id, Integer ], [ :name, String ] ])
+TIMES = 5_000
 
-RELATIONS = {}
-
-COUNTS.each do |count|
-  RELATIONS[count] = Relation.new(
-    HEADER,
-    (1..count).map { |n| Tuple.new(HEADER, [ n, NAME ]) }
-  )
-end
-
-def each_count
-  COUNTS.each do |count|
-    relation = RELATIONS[count].dup
-    yield(relation.to_a, relation, count)
-  end
-end
+header   = Relation::Header.new([ [ :id, Integer ], [ :name, String ] ])
+array    = (1..100).map { |n| Tuple.new(header, [ n, 'Dan Kubb' ]) }
+relation = Relation.new(header, array)
+join     = Relation.new([ [ :id, Integer, ], [ :age, Integer ] ], [ [ 1, 34 ] ])
+product  = Relation.new([ [ :age, Integer ] ], [ [ 34 ] ])
+ordered  = relation.order(relation.header)
 
 RBench.run(TIMES) do
   column :ruby,    :title => 'Ruby'
   column :veritas, :title => 'Veritas'
   column :diff,    :title => 'Diff', :compare => [ :ruby, :veritas ]
 
-  each_count do |array, relation, count|
-    report "enumeration (#{count} tuples)" do
-      ruby    { array.each {} }
-      veritas { relation.each {} }
-    end
+  report 'each' do
+    ruby    { array.each {} }
+    veritas { relation.each {} }
   end
 
-  each_count do |array, relation, count|
-    report "projection (#{count} tuples)" do
-      ruby    { array.map { |tuple| [ tuple[:id] ] }.each {} }
-      veritas { relation.project([ :id ]).each {} }
-    end
+  report 'projection' do
+    ruby    { array.map { |tuple| [ tuple[:id] ] }.each {} }
+    veritas { relation.project([ :id ]).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "removal (#{count} tuples)" do
-      ruby    { array.map { |tuple| [ tuple[:id] ] }.each {} }
-      veritas { relation.remove([ :name ]).each {} }
-    end
+  report 'removal' do
+    ruby    { array.map { |tuple| [ tuple[:id] ] }.each {} }
+    veritas { relation.remove([ :name ]).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "restriction (#{count} tuples)" do
-      ruby    { array.select { |tuple| tuple[:id] >= 1 }.each {} }
-      veritas { relation.restrict { |r| r[:id].gte(1) }.each {} }
-    end
+  report 'restriction' do
+    ruby    { array.select { |tuple| tuple[:id] >= 1 }.each {} }
+    veritas { relation.restrict { |r| r[:id].gte(1) }.each {} }
   end
 
-  each_count do |array, relation, count|
-    report "join (#{count} tuples)" do
-      veritas { relation.join(Relation.new([ [ :id, Integer, ], [ :age, Integer ] ], [ [ 1, 34 ] ])).each {} }
-    end
+  report 'join' do
+    veritas { relation.join(join).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "product (#{count} tuples)" do
-#      ruby    { array.product(array).each {} }  # uses too much memory and crashes
-      veritas { relation.product(Relation.new([ [ :age, Integer ] ], [ [ 34 ] ])).each {} }
-    end
+  report 'product' do
+    ruby    { array.product(array).each {} } if Array.method_defined?(:product)
+    veritas { relation.product(product).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "intersection (#{count} tuples)" do
-      ruby    { (array & array).each {} }
-      veritas { relation.intersect(relation).each {} }
-    end
+  report 'intersection' do
+    ruby    { (array & array).each {} }
+    veritas { relation.intersect(relation).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "union (#{count} tuples)" do
-      ruby    { (array | array).each {} }
-      veritas { relation.union(relation).each {} }
-    end
+  report 'union' do
+    ruby    { (array | array).each {} }
+    veritas { relation.union(relation).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "difference (#{count} tuples)" do
-      ruby    { (array - array).each {} }
-      veritas { relation.difference(relation).each {} }
-    end
+  report 'difference' do
+    ruby    { (array - array).each {} }
+    veritas { relation.difference(relation).each {} }
   end
 
-  each_count do |array, relation, count|
-    report "order (#{count} tuples)" do
-      ruby    { array.sort_by { |tuple| [ -tuple[:id], tuple[:name] ] }.each {} }
-      veritas { relation.order { |r| [ r[:id].desc, r[:name] ] }.each {} }
-    end
+  report 'order' do
+    ruby    { array.sort_by { |tuple| [ -tuple[:id], tuple[:name] ] }.each {} }
+    veritas { relation.order { |r| [ r[:id].desc, r[:name] ] }.each {} }
   end
 
-  each_count do |array, relation, count|
-    relation = relation.order(relation.header)
-
-    report "limit (#{count} tuples)" do
-      ruby    { array.take(count).each {} }
-      veritas { relation.limit(count).each {} }
-    end
+  report 'limit' do
+    ruby    { array.take(1).each {} }
+    veritas { ordered.limit(1).each {} }
   end
 
-  each_count do |array, relation, count|
-    relation = relation.order(relation.header)
-
-    report "offset (#{count} tuples)" do
-      ruby    { array.drop(1).each {} }
-      veritas { relation.offset(1).each {} }
-    end
+  report 'offset' do
+    ruby    { array.drop(1).each {} }
+    veritas { ordered.offset(1).each {} }
   end
 
-  each_count do |array, relation, count|
-    relation = relation.order(relation.header)
-
-    report "first (#{count} tuples)" do
-      ruby    { array.first(count).each {} }
-      veritas { relation.first(count).each {} }
-    end
+  report 'first' do
+    ruby    { array.first(1).each {} }
+    veritas { ordered.first(1).each {} }
   end
 
-  each_count do |array, relation, count|
-    relation = relation.order(relation.header)
-
-    report "last (#{count} tuples)" do
-      ruby    { array.last(count).each {} }
-      veritas { relation.last(count).each {} }
-    end
+  report 'last' do
+    ruby    { array.last(1).each {} }
+    veritas { ordered.last(1).each {} }
   end
 
-  each_count do |array, relation, count|
-    relation = relation.order(relation.header)
-
-    report "reverse (#{count} tuples)" do
-      ruby    { array.reverse.each {} }
-      veritas { relation.reverse.each {} }
-    end
+  report 'reverse' do
+    ruby    { array.reverse.each {} }
+    veritas { ordered.reverse.each {} }
   end
 end
