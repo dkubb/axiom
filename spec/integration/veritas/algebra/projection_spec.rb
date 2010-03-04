a0680ce3221718612b5e85f66f286d7193a18d57
Dan Kubb <dan.kubb@gmail.com>
2010-03-04 07:58:17 +0100

    Added integration spec fro projection and predicate interaction

---
+++ spec/integration/veritas/algebra/projection_spec.rb
@@ -0,0 +1,31 @@
+require File.expand_path('../../../../spec_helper', __FILE__)
+
+describe 'Veritas::Algebra::Projection' do
+  describe 'remove attributes in predicate' do
+    before do
+      @left  = Relation.new([ [ :id, Integer ] ], (1..100).map { |n| [ n ] })
+      @right = Relation.new([ [ :name, String ] ], [ [ 'Dan Kubb' ], [ 'John Doe' ], [ 'Jane Doe' ] ])
+
+      @relation = @left * @right
+
+      @restriction = @relation.restrict { |r| r[:id].gte(1).and(r[:id].lte(10)).and(r[:name].eq('Dan Kubb')) }
+    end
+
+    subject { @restriction.project([ :name ]) }
+
+    it 'should remove the predicates with the removed attributes' do
+      subject.predicate.should == @relation[:name].eq('Dan Kubb')
+    end
+
+    it 'should return a relation with a single tuple' do
+      subject.should == [ [ 'Dan Kubb' ] ]
+    end
+
+    it 'should have a predicate that matches every tuple' do
+      projection = subject
+      predicate  = projection.predicate
+
+      projection.should be_all { |tuple| predicate.call(tuple) }
+    end
+  end
+end
