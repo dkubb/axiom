7593efa6e1fae383e100b2b51a6eac99dadb8789
Dan Kubb <dan.kubb@gmail.com>
2010-05-11 14:11:28 -0700

    Use #eql? to compare predicates
    * In the future Expression#== will be overridden to return an Equality
      predicate

--- spec/unit/veritas/algebra/rename/optimize_spec.rb
+++ spec/unit/veritas/algebra/rename/optimize_spec.rb
@@ -135,7 +135,7 @@ describe 'Veritas::Algebra::Rename#optimize' do
 
     its(:header) { should == [ [ :other_id, Integer ], [ :name, String ] ] }
 
-    its(:predicate) { should == rename[:other_id].eq(1) }
+    its(:predicate) { should eql(rename[:other_id].eq(1)) }
 
     it 'returns an equivalent relation to the unoptimized operation' do
       should == rename
