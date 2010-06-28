fd6d5ab4933e08096793d57c50f47f60b42e5f60
Dan Kubb <dan.kubb@gmail.com>
2010-06-27 21:38:13 -0700

    Ensure spec was testing behavior when there are two valid entries

--- spec/unit/veritas/logic/predicate/inclusion/optimize_spec.rb
+++ spec/unit/veritas/logic/predicate/inclusion/optimize_spec.rb
@@ -96,10 +96,10 @@ describe 'Veritas::Logic::Predicate::Inclusion#optimize' do
     end
 
     context 'that is not empty after filtering invalid values' do
-      let(:right)     { [ 'a', 1 ]                                   }
+      let(:right)     { [ 'a', 1, 2 ]                                }
       let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
 
-      it { should eql(left.in([ 1 ])) }
+      it { should eql(left.in([ 1, 2 ])) }
 
       it_should_behave_like 'an optimize method'
     end
