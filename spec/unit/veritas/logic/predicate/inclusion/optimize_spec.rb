56f470cc60d4cce3a514ac20efd477dcd5b481b5
Dan Kubb <dan.kubb@gmail.com>
2010-06-27 21:35:44 -0700

    Removed context that is always true (left is always an attribute, for now)

--- spec/unit/veritas/logic/predicate/inclusion/optimize_spec.rb
+++ spec/unit/veritas/logic/predicate/inclusion/optimize_spec.rb
@@ -5,61 +5,10 @@ describe 'Veritas::Logic::Predicate::Inclusion#optimize' do
 
   let(:left) { Attribute::Integer.new(:id) }
 
-  context 'left is an attribute' do
-    context 'right is a Range' do
-      context 'that is inclusive' do
-        context 'and empty' do
-          let(:right)     { 1..0                                         }
-          let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
-
-          it { should equal(Logic::Proposition::False.instance) }
-
-          it_should_behave_like 'an optimize method'
-        end
-
-        context 'and not empty' do
-          let(:right)     { 1..10                                        }
-          let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
-
-          it { should equal(inclusion) }
-
-          it_should_behave_like 'an optimize method'
-        end
-      end
-
-      context 'that is exclusive' do
-        context 'and empty' do
-          let(:right)     { 1...1                                        }
-          let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
-
-          it { should equal(Logic::Proposition::False.instance) }
-
-          it_should_behave_like 'an optimize method'
-        end
-
-        context 'and not empty' do
-          let(:right)     { 1...10                                       }
-          let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
-
-          it 'changes the Range to be inclusive' do
-            should eql(left.in(1..9))
-          end
-
-          it_should_behave_like 'an optimize method'
-        end
-      end
-
-      context 'using an attribute that is not comparable' do
-        let(:left)      { Attribute::String.new(:string)                  }
-        let(:inclusion) { Logic::Predicate::Inclusion.new(left, 'a'..'z') }
-
-        it { should equal(Logic::Proposition::False.instance) }
-
-        it_should_behave_like 'an optimize method'
-      end
-
-      context 'that is greater than the left range' do
-        let(:right)     { 2**31..2**31                                 }
+  context 'right is a Range' do
+    context 'that is inclusive' do
+      context 'and empty' do
+        let(:right)     { 1..0                                         }
         let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
 
         it { should equal(Logic::Proposition::False.instance) }
@@ -67,19 +16,19 @@ describe 'Veritas::Logic::Predicate::Inclusion#optimize' do
         it_should_behave_like 'an optimize method'
       end
 
-      context 'that is less than the left range' do
-        let(:right)     { -1..-1                                       }
+      context 'and not empty' do
+        let(:right)     { 1..10                                        }
         let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
 
-        it { should equal(Logic::Proposition::False.instance) }
+        it { should equal(inclusion) }
 
         it_should_behave_like 'an optimize method'
       end
     end
 
-    context 'right is an Enumerable' do
-      context 'that is empty' do
-        let(:right)     { []                                           }
+    context 'that is exclusive' do
+      context 'and empty' do
+        let(:right)     { 1...1                                        }
         let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
 
         it { should equal(Logic::Proposition::False.instance) }
@@ -87,32 +36,81 @@ describe 'Veritas::Logic::Predicate::Inclusion#optimize' do
         it_should_behave_like 'an optimize method'
       end
 
-      context 'that is empty after filtering invalid values' do
-        let(:right)     { [ 'a' ]                                      }
+      context 'and not empty' do
+        let(:right)     { 1...10                                       }
         let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
 
-        it { should equal(Logic::Proposition::False.instance) }
-
-        it_should_behave_like 'an optimize method'
-      end
-
-      context 'that is not empty after filtering invalid values' do
-        let(:right)     { [ 'a', 1 ]                                   }
-        let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
-
-        it { should eql(left.in([ 1 ])) }
+        it 'changes the Range to be inclusive' do
+          should eql(left.in(1..9))
+        end
 
         it_should_behave_like 'an optimize method'
       end
     end
 
-    context 'right is a nil' do
-      let(:right)     { nil                                          }
+    context 'using an attribute that is not comparable' do
+      let(:left)      { Attribute::String.new(:string)                  }
+      let(:inclusion) { Logic::Predicate::Inclusion.new(left, 'a'..'z') }
+
+      it { should equal(Logic::Proposition::False.instance) }
+
+      it_should_behave_like 'an optimize method'
+    end
+
+    context 'that is greater than the left range' do
+      let(:right)     { 2**31..2**31                                 }
       let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
 
       it { should equal(Logic::Proposition::False.instance) }
 
       it_should_behave_like 'an optimize method'
     end
+
+    context 'that is less than the left range' do
+      let(:right)     { -1..-1                                       }
+      let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
+
+      it { should equal(Logic::Proposition::False.instance) }
+
+      it_should_behave_like 'an optimize method'
+    end
+  end
+
+  context 'right is an Enumerable' do
+    context 'that is empty' do
+      let(:right)     { []                                           }
+      let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
+
+      it { should equal(Logic::Proposition::False.instance) }
+
+      it_should_behave_like 'an optimize method'
+    end
+
+    context 'that is empty after filtering invalid values' do
+      let(:right)     { [ 'a' ]                                      }
+      let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
+
+      it { should equal(Logic::Proposition::False.instance) }
+
+      it_should_behave_like 'an optimize method'
+    end
+
+    context 'that is not empty after filtering invalid values' do
+      let(:right)     { [ 'a', 1 ]                                   }
+      let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
+
+      it { should eql(left.in([ 1 ])) }
+
+      it_should_behave_like 'an optimize method'
+    end
+  end
+
+  context 'right is a nil' do
+    let(:right)     { nil                                          }
+    let(:inclusion) { Logic::Predicate::Inclusion.new(left, right) }
+
+    it { should equal(Logic::Proposition::False.instance) }
+
+    it_should_behave_like 'an optimize method'
   end
 end
