bb921e8c4edec17cb958c3817d44a3054a97461c
Dan Kubb <dan.kubb@gmail.com>
2010-06-29 07:02:21 -0700

    Removed Range#to_exclusive
    * Trying to eliminate core_ext patches, and this method was not used
      internally, so removing it early.

--- lib/veritas/core_ext/range.rb
+++ lib/veritas/core_ext/range.rb
@@ -7,14 +7,6 @@ class Range
     end
   end unless method_defined? :to_inclusive
 
-  def to_exclusive
-    if exclude_end?
-      self
-    else
-      self.class.new(first, last.succ, true)
-    end
-  end unless method_defined? :to_exclusive
-
   def overlaps?(other)
     include?(other.first) || other.include?(first)
   end unless method_defined? :overlaps?
