82b398c953ee85fc6ca79b5797476225ac86eb74
Dan Kubb <dan.kubb@gmail.com>
2010-06-30 18:13:14 -0700

    Extracted setup code from subject

--- spec/unit/veritas/logic/predicate/inclusion/methods/include_spec.rb
+++ spec/unit/veritas/logic/predicate/inclusion/methods/include_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 require File.expand_path('../fixtures/classes', __FILE__)
 
 describe 'Veritas::Logic::Predicate::Inclusion::Methods#include' do
-  subject { expression.include([ true, false ]) }
+  subject { expression.include(enumerable) }
 
   let(:expression) { InclusionMethodsSpecs::Object.new }
+  let(:enumerable) { [ true, false ]                   }
 
   it { should be_kind_of(Logic::Predicate::Inclusion) }
 end
