d8e3c931f9ecc578171784a3cc46c6735c8e182f
Dan Kubb <dan.kubb@gmail.com>
2010-07-22 10:17:31 -0700

    Make sure Relation::Materialized#empty? does not call #each on the body

--- spec/unit/veritas/relation/materialized/empty_spec.rb
+++ spec/unit/veritas/relation/materialized/empty_spec.rb
@@ -3,17 +3,28 @@ require 'spec_helper'
 describe 'Veritas::Relation::Materialized#empty?' do
   subject { relation.empty? }
 
-  let(:header) { [ [ :id, Integer ] ] }
+  let(:header)   { [ [ :id, Integer ] ]                     }
+  let(:relation) { Relation::Materialized.new(header, body) }
 
   context 'with a body containing no entries' do
-    let(:relation) { Relation::Materialized.new(header, []) }
+    let(:body) { [] }
 
     it { should be(true) }
+
+    it 'does not execute body#each' do
+      body.should_not_receive(:each)
+      subject
+    end
   end
 
   context 'with a body containing an entry' do
-    let(:relation) { Relation::Materialized.new(header, [ [ 1 ] ]) }
+    let(:body) { [ [ 1 ] ] }
 
     it { should be(false) }
+
+    it 'does not execute body#each' do
+      body.should_not_receive(:each)
+      subject
+    end
   end
 end
