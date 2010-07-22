db04d18b4cb13a7b52a0fadc6270b679ce176319
Dan Kubb <dan.kubb@gmail.com>
2010-07-21 21:24:21 -0700

    Updated Relation#empty? specs to use an Enumerator
    * Ensure that Relation#empty? is being called, not
      Relation::Materialized#empty?.  When an Array is passed in to
      Relation.new, a Relation::Materialized instance is returned and
      the #empty? method is called on it. While it does inherit the
      method from Relation, if I were to add an #empty? method to
      Relation::Materialized the specs would no longer test the intended
      object. Using an Enumerable avoids this and ensures that a
      Relation instance is used.

--- spec/unit/veritas/relation/empty_spec.rb
+++ spec/unit/veritas/relation/empty_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe 'Veritas::Relation#empty?' do
   subject { relation.empty? }
 
-  let(:header) { [ [ :id, Integer ] ] }
+  let(:header)   { [ [ :id, Integer ] ]       }
+  let(:relation) { Relation.new(header, body) }
 
   context 'with a body containing no entries' do
-    let(:relation) { Relation.new(header, []) }
+    let(:body) { [].each }  # use an Enumerator
 
     it { should be(true) }
   end
 
   context 'with a body containing an entry' do
-    let(:relation) { Relation.new(header, [ [ 1 ] ]) }
+    let(:body) { [ [ 1 ] ].each }  # use an Enumerator
 
     it { should be(false) }
   end
