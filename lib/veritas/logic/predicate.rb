d3ed6bbfa6f5548468a14aa0ba7bc7a455c675a4
Dan Kubb <dan.kubb@gmail.com>
2010-08-07 01:19:59 -0700

    Minor code formatting change

--- lib/veritas/logic/predicate.rb
+++ lib/veritas/logic/predicate.rb
@@ -12,7 +12,9 @@ module Veritas
       end
 
       def project(attributes)
-        left, right = self.left, self.right
+        left  = self.left
+        right = self.right
+
         return if left.kind_of?(Attribute)  && !attributes.include?(left)
         return if right.kind_of?(Attribute) && !attributes.include?(right)
         super
