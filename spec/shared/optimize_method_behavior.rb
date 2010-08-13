6537ad64d5e4450802cd4c90ba4f03fbeaf15037
Dan Kubb <dan.kubb@gmail.com>
2010-08-12 21:59:49 -0700

    Minor improvement to variable naming in shared spec

--- spec/shared/optimize_method_behavior.rb
+++ spec/shared/optimize_method_behavior.rb
@@ -2,8 +2,8 @@ shared_examples_for 'an optimize method' do
   it_should_behave_like 'an idempotent method'
 
   it 'does not optimize further' do
-    object = subject
-    object.optimize.should equal(object)
+    optimized = subject
+    optimized.optimize.should equal(optimized)
   end
 
   it 'returns an optimized object' do
