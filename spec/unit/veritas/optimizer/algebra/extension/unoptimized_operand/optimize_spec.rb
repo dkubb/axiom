333be5d52e02d780b09550baf50b8ff76a9f1411
Dan Kubb <dan.kubb@gmail.com>
2010-09-16 13:07:37 -0700

    Minor formatting fix

--- spec/unit/veritas/optimizer/algebra/extension/unoptimized_operand/optimize_spec.rb
+++ spec/unit/veritas/optimizer/algebra/extension/unoptimized_operand/optimize_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe 'Veritas::Optimizer::Algebra::Extension::UnoptimizedOperand#optimize' do
   subject { object.optimize }
 
-  let(:klass)      { Optimizer::Algebra::Extension::UnoptimizedOperand }
-  let(:header)     { Relation::Header.new([ [ :id, Integer ] ])        }
-  let(:base)       { Relation.new(header, [ [ 1 ] ].each)              }
-  let(:attribute)  { Attribute::Object.new(:text)                      }
-  let(:function)   { lambda { |tuple| 1 }                              }
-  let(:operand)    { base.project(header)                              }
-  let(:relation)   { operand.extend { |r| r.add(attribute, function) } }
-  let(:object)     { klass.new(relation)                               }
+  let(:klass)     { Optimizer::Algebra::Extension::UnoptimizedOperand }
+  let(:header)    { Relation::Header.new([ [ :id, Integer ] ])        }
+  let(:base)      { Relation.new(header, [ [ 1 ] ].each)              }
+  let(:attribute) { Attribute::Object.new(:text)                      }
+  let(:function)  { lambda { |tuple| 1 }                              }
+  let(:operand)   { base.project(header)                              }
+  let(:relation)  { operand.extend { |r| r.add(attribute, function) } }
+  let(:object)    { klass.new(relation)                               }
 
   before do
     object.operation.should be_kind_of(Algebra::Extension)
