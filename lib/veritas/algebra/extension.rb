531f6f53e04b19fb13da27ed8b2d0ee1a33ae1e4
Dan Kubb <dan.kubb@gmail.com>
2010-09-20 22:05:33 -0700

    Added YARD docs for Extension

--- lib/veritas/algebra/extension.rb
+++ lib/veritas/algebra/extension.rb
@@ -1,15 +1,44 @@
 module Veritas
   module Algebra
+
+    # Extend a relation to include calculated attributes
     class Extension < Relation
       include Relation::Operation::Unary
 
+      # The extensions for the relation
+      #
+      # @return [Hash]
+      #
+      # @api private
       attr_reader :extensions
 
+      # Instantiate a new Extension
+      #
+      # @example
+      #   extension = Extension.new(operand, extensions)
+      #
+      # @param [Relation] operand
+      #   the relation to extend
+      # @param [Hash] extensions
+      #   the extensions to add
+      #
+      # @return [Extension]
+      #
+      # @api public
       def self.new(operand, extensions)
         assert_unique_header(operand, extensions)
         super
       end
 
+      # Assert the extensions are uniquely named
+      #
+      # @return [undefined]
+      #
+      # @raise [DuplicateHeaderName]
+      #   raised if an extensions is named the same as an existing
+      #   attribute in the header
+      #
+      # @api private
       def self.assert_unique_header(operand, extensions)
         header     = operand.header
         duplicates = extensions.keys.select { |attribute| header[attribute] }.sort
@@ -20,12 +49,36 @@ module Veritas
 
       private_class_method :assert_unique_header
 
+      # Initialize an Extension
+      #
+      # @param [Relation] operand
+      #   the relation to extend
+      # @param [Hash] extensions
+      #   the extensions to add
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(operand, extensions)
         super(operand)
         @extensions  = extensions
         @header     |= @extensions.keys
       end
 
+      # Iterate over each tuple in the set
+      #
+      # @example
+      #   extension = Extension.new(operand, extensions)
+      #   extension.each { |tuple| ... }
+      #
+      # @yield [tuple]
+      #
+      # @yieldparam [Tuple] tuple
+      #   each tuple in the set
+      #
+      # @return [self]
+      #
+      # @api public
       def each
         header     = self.header
         extensions = self.extensions.values
@@ -34,6 +87,19 @@ module Veritas
       end
 
       module Methods
+
+        # Return an extended relation
+        #
+        # @example
+        #   extension = relation.extend do |expression|
+        #     expression.add(:total, expression[:unit_price] * expression[:quantity])
+        #   end
+        #
+        # @param [Relation] other
+        #
+        # @return [Difference]
+        #
+        # @api public
         def extend(&block)
           evaluator = Evaluator::Expression.new(&block)
           Extension.new(self, evaluator.expressions)
