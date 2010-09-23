2abb4d7069227a894b495ab91e3385a931068546
Dan Kubb <dan.kubb@gmail.com>
2010-09-23 11:59:06 -0700

    Added YARD docs for Projection

--- lib/veritas/algebra/projection.rb
+++ lib/veritas/algebra/projection.rb
@@ -1,14 +1,40 @@
 module Veritas
   module Algebra
+
+    # Specify only specific attributes to keep in the relation
     class Projection < Relation
       include Relation::Operation::Unary
 
+      # Initialize a Projection
+      #
+      # @param [Relation] operand
+      #   the relation to project
+      # @param [#to_ary] attributes
+      #   the attributes to keep in the header
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(operand, attributes)
         super(operand)
         @header     = @header.project(attributes.to_ary)
         @directions = @directions.project(@header)
       end
 
+      # Iterate over each tuple in the set
+      #
+      # @example
+      #   projection = Projection.new(operand, attributes)
+      #   projection.each { |tuple| ... }
+      #
+      # @yield [tuple]
+      #
+      # @yieldparam [Tuple] tuple
+      #   each tuple in the set
+      #
+      # @return [self]
+      #
+      # @api public
       def each
         seen = {}
         operand.each do |tuple|
@@ -19,10 +45,35 @@ module Veritas
       end
 
       module Methods
+
+        # Return a relation with only the attributes specified
+        #
+        # @example
+        #   projection = relation.project([ :a, b, c ])
+        #
+        # @param [#to_ary] attributes
+        #   the attributes to keep in the header
+        #
+        # @return [Projection]
+        #   a relation containing only the specified attributes
+        #
+        # @api public
         def project(attributes)
           project_relation(self, attributes)
         end
 
+        # Return a relation with attributes not specified
+        #
+        # @example
+        #   projection = relation.remove([ :a, b, c ])
+        #
+        # @param [#to_ary] attributes
+        #   the attributes to remove from the header
+        #
+        # @return [Projection]
+        #   a relation containing everything except the specified attributes
+        #
+        # @api public
         def remove(attributes)
           project(header - project_header(attributes))
         end
