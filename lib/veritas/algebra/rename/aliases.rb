90c92bafc62e1ae06d309dd36d91087a335ba3f4
Dan Kubb <dan.kubb@gmail.com>
2010-09-27 21:03:13 -0700

    Added YARD docs for Rename::Aliases

--- lib/veritas/algebra/rename/aliases.rb
+++ lib/veritas/algebra/rename/aliases.rb
@@ -7,14 +7,44 @@ module Veritas
 
         inheritable_alias(:| => :union)
 
+        # Initialize rename aliases
+        #
+        # @param [#to_hash] aliases
+        #   the old and new attributes
+        #
+        # @return [undefined]
+        #
+        # @api public
         def initialize(aliases)
           @aliases = Immutable.freeze_value(aliases.to_hash)
         end
 
+        # Lookup the new attribute given the old attribute
+        #
+        # @example
+        #   new_attribute = aliases[old_attribute]
+        #
+        # @param [Attribute] attribute
+        #   the old attribute
+        #
+        # @return [Attribute]
+        #
+        # @api public
         def [](attribute)
           @aliases.fetch(attribute, attribute)
         end
 
+        # Union the aliases with another set of aliases
+        #
+        # @example
+        #   new_aliases = aliases.union(other)
+        #
+        # @param [Aliases] other
+        #   the aliases to union with
+        #
+        # @return [Aliases]
+        #
+        # @api public
         def union(other)
           other_aliases = other.to_hash.dup
           inverted      = other_aliases.invert
@@ -32,33 +62,106 @@ module Veritas
           self.class.new(other_aliases)
         end
 
+        # Iterate over each old and new attribute
+        #
+        # @example
+        #   aliases = Aliases.new(old => new)
+        #   aliases.each { |old, new| ... }
+        #
+        # @yield [old, new]
+        #
+        # @yieldparam [Attribute] old
+        #   the old attribute
+        # @yieldparam [Attribute] new
+        #   the new attribute
+        #
+        # @return [self]
+        #
+        # @api public
         def each(&block)
           @aliases.each(&block)
           self
         end
 
+        # Test if there are no aliases
+        #
+        # @example
+        #   aliases.empty?  # => true or false
+        #
+        # @return [Boolean]
+        #
+        # @api public
         def empty?
           @aliases.empty?
         end
 
+        # Compare the aliases with other aliases for equivalency
+        #
+        # @example
+        #   aliases == other  # => true or false
+        #
+        # @param [Aliases] other
+        #
+        # @return [Boolean]
+        #
+        # @api public
         def ==(other)
           (kind_of?(other.class) || other.kind_of?(self.class)) &&
           to_hash == other.to_hash
         end
 
+        # Compare the aliases with other aliases for equality
+        #
+        # @example
+        #   aliases.eql?(other)  # => true or false
+        #
+        # @param [Aliases] other
+        #
+        # @return [Boolean]
+        #
+        # @api public
         def eql?(other)
           instance_of?(other.class) &&
           to_hash.eql?(other.to_hash)
         end
 
+        # Return the hash of the aliases
+        #
+        # @example
+        #   numeric_hash = aliases.hash
+        #
+        # @return [Integer]
+        #
+        # @api public
         def hash
           @aliases.hash
         end
 
+        # Convert the aliases to a Hash
+        #
+        # @example
+        #   hash = aliases.to_hash
+        #
+        # @return [Hash]
+        #
+        # @api public
         def to_hash
           @aliases
         end
 
+        # Coerce a Hash of old and new attributes into Aliases
+        #
+        # @example
+        #    aliases = Aliases.coerce(attributes, :old => :new)
+        #
+        # @param [Header] attributes
+        #   the header containing the old attributes
+        # @param [Aliases, #map] aliases
+        #   the aliases to coerce
+        #
+        # @return [Aliases]
+        #
+        # @api public
         def self.coerce(attributes, aliases)
           return aliases if aliases.kind_of?(Aliases)
           renames = aliases.map do |old_attr, new_attr|
@@ -67,6 +170,18 @@ module Veritas
           new(Hash[renames].freeze)
         end
 
+        # Coerce old and new attributes into Attribute objects
+        #
+        # @param [Header] attributes
+        #   the header containing the old attributes
+        # @param [Symbol, Attribute] old_attr
+        #   the old attribute name or Attribute object
+        # @param [Symbol, Attribute] new_attr
+        #   the new attribute name or Attribute object
+        #
+        # @return [Array(Attribute, Attribute)]
+        #
+        # @api private
         def self.coerce_alias_pair(attributes, old_attr, new_attr)
           old_attr = attributes[old_attr]      if old_attr.kind_of?(Symbol)
           new_attr = old_attr.rename(new_attr) if new_attr.kind_of?(Symbol)
