4da67f1c3c0ef305c7d270ce449668f147991765
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 15:09:18 -0700

    Added YARD docs for Attribute

--- lib/veritas/attribute.rb
+++ lib/veritas/attribute.rb
@@ -16,61 +16,196 @@ require 'veritas/attribute/time'
 require 'veritas/attribute/string'
 
 module Veritas
+
+  # Abstract base class representing a type of data in a relation tuple
   class Attribute
     include AbstractClass, Immutable, ::Comparable
 
-    attr_reader :name, :options
+    # The attribute name
+    #
+    # @return [Symbol]
+    #
+    # @api private
+    attr_reader :name
 
+    # The attribute options
+    #
+    # @return [Hash]
+    #
+    # @api private
+    attr_reader :options
+
+    # Initialize an Attribute
+    #
+    # @param [#to_sym] name
+    #   the attribute name
+    # @param [#to_hash] options
+    #   the options for the attribute
+    # @option options [Boolean] :required (true)
+    #   if true, then the value cannot be nil
+    #
+    # @return [undefined]
+    #
+    # @api private
     def initialize(name, options = {})
       @name     = name.to_sym
       @options  = Immutable.freeze_object(options.to_hash)
       @required = @options.fetch(:required, true)
     end
 
+    # Extract the value corresponding to this attribute from a tuple
+    #
+    # @example
+    #   value = attribute.call(tuple)
+    #
+    # @param [Tuple] tuple
+    #   the tuple to extract the value from
+    #
+    # @return [Object]
+    #
+    # @api public
     def call(tuple)
       tuple[self]
     end
 
+    # Rename an attribute
+    #
+    # @example
+    #   new_attribute = attribute.rename(new_name)
+    #
+    # @param [Symbol] new_name
+    #   the new name to rename the attribute to
+    #
+    # @return [Attribute]
+    #
+    # @api public
     def rename(new_name)
       name.equal?(new_name) ? self : self.class.new(new_name, options)
     end
 
+    # Test if the attribute is required
+    #
+    # @example
+    #   attribute.required?  # => true or false
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def required?
       @required
     end
 
+    # Test if a value is the correct primitive type
+    #
+    # @example
+    #   attribute.valid_primitive?(value)  # => true or false
+    #
+    # @param [Object] value
+    #   the value to test
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def valid_primitive?(value)
       value.kind_of?(self.class.primitive)
     end
 
+    # Test if the value matches the attribute constraints
+    #
+    # @example
+    #   attribute.valid_value?(value)  # => true or false
+    #
+    # @param [Object] value
+    #   the value to test
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def valid_value?(value)
       valid_or_optional?(value) { valid_primitive?(value) }
     end
 
+    # Test if the attribute can be joined with the other attribute
+    #
+    # @example
+    #   attribute.joinable?(other)  # => true or false
+    #
+    # @param [Attribute] other
+    #   the other attribute to test
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def joinable?(other)
       kind_of?(other.class) || other.kind_of?(self.class)
     end
 
+    # Compare the attribute with other attribute for equivalency
+    #
+    # @example
+    #   attribute == other  # => true or false
+    #
+    # @param [Attribute] other
+    #   the other attribute to compare with
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def ==(other)
       other = Attribute.coerce(other)
       name.equal?(other.name) &&
       options == other.options
     end
 
+    # Compare the attribute with other attribute for equality
+    #
+    # @example
+    #   attribute.eql?(other)  # => true or false
+    #
+    # @param [Attribute] other
+    #   the other attribute to compare with
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def eql?(other)
       instance_of?(other.class) &&
       name.equal?(other.name)   &&
       options.eql?(other.options)
     end
 
+    # Return the hash of the attribute
+    #
+    # @example
+    #   numeric_hash = attribute.hash
+    #
+    # @return [Integer]
+    #
+    # @api public
     def hash
       name.hash ^ options.hash
     end
 
+    # Return a string representing the attribute
+    #
+    # @example
+    #   attribute.inspect  # => "<Attribute::Integer name: id>"
+    #
+    # @return [String]
+    #
+    # @api public
     def inspect
       "<#{self.class.name.sub(/\AVeritas::/, '')} name: #{name}>"
     end
 
+    # Coerce an object into an Attribute
+    #
+    # @param [Attribute, #to_ary, #to_sym] object
+    #   the object to coerce
+    #
+    # @return [Attribute]
+    #
+    # @api private
     def self.coerce(object)
       if object.kind_of?(self)
         object
@@ -81,6 +216,14 @@ module Veritas
       end
     end
 
+    # Extract the attribute name from the object
+    #
+    # @param [Attribute, #to_ary, #to_sym] object
+    #   the object to extract a name from
+    #
+    # @return [Symbol]
+    #
+    # @api private
     def self.name_from(object)
       if object.kind_of?(self)
         object.name
@@ -93,6 +236,16 @@ module Veritas
 
   private
 
+    # Test that the value is either valid or optional
+    #
+    # @param [Object] value
+    #   the value to test
+    #
+    # @yield [] block representing attribute constraint
+    #
+    # @return [Boolean]
+    #
+    # @api private
     def valid_or_optional?(value)
       value.nil? ? !required? : yield
     end
