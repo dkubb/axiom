d0e7d562c081ccc92d27c9bd9455fd4e9922cf5f
Dan Kubb <dan.kubb@gmail.com>
2010-09-29 15:36:18 -0700

    Added YARD docs for Operation::Unary

--- lib/veritas/support/operation/unary.rb
+++ lib/veritas/support/operation/unary.rb
@@ -3,17 +3,51 @@ module Veritas
     module Unary
       include Immutable
 
+      # The operand to the operation
+      #
+      # @example
+      #   operand = unary.operand
+      #
+      # @return [Object]
+      #
+      # @api public
       attr_reader :operand
 
+      # Initialize Unary Operation
+      #
+      # @param [Object] operand
+      #   the operand for the operation
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(operand)
         @operand = operand
       end
 
+      # Compare the operation with the other operation for equality
+      #
+      # @example
+      #   unary.eql?(other)  # => true or false
+      #
+      # @param [Object] other
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def eql?(other)
         instance_of?(other.class) &&
         operand.eql?(other.operand)
       end
 
+      # Return the hash of the operand
+      #
+      # @example
+      #   numeric_hash = unary.hash
+      #
+      # @return [Integer]
+      #
+      # @api public
       def hash
         operand.hash
       end
