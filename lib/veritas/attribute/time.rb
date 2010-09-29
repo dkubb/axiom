015bad060e90146f2127ca3f80922e9f0cf216bf
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 22:32:53 -0700

    Added YARD docs for Attribute::Time

--- lib/veritas/attribute/time.rb
+++ lib/veritas/attribute/time.rb
@@ -1,14 +1,32 @@
 module Veritas
   class Attribute
+
+    # Represents a Time value in a relation tuple
     class Time < Object
       include Comparable
 
       DEFAULT_RANGE = (::Time.at(0)..::Time.at(2**31-1)).freeze
 
+      # The Time primitive
+      #
+      # @example
+      #   Time.primitive  # => ::Time
+      #
+      # @return [::Time]
+      #
+      # @api public
       def self.primitive
         ::Time
       end
 
+      # The Time range for a valid value
+      #
+      # @example
+      #   Time.range  # => ::Time.utc(*from)..::Time.utc(*to)
+      #
+      # @return [Range<::Time>]
+      #
+      # @api public
       def range
         DEFAULT_RANGE
       end
