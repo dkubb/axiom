57f604f88bb260e93a1dcd8ac3024bdbe5ef017e
Dan Kubb <dan.kubb@gmail.com>
2010-09-29 08:57:10 -0700

    Added YARD docs for Range

--- lib/veritas/core_ext/range.rb
+++ lib/veritas/core_ext/range.rb
@@ -1,4 +1,14 @@
+# Extend Range with methods to normalize and find overlapping ranges
 class Range
+
+  # Returns an inclusive Range
+  #
+  # @example
+  #   inclusive = range.to_inclusive
+  #
+  # @return [Range]
+  #
+  # @api public
   def to_inclusive
     if exclude_end?
       self.class.new(first, last.pred)
@@ -7,6 +17,17 @@ class Range
     end
   end unless method_defined? :to_inclusive
 
+  # Compare the range with another range to see if they overlap
+  #
+  # @example
+  #   range.overlaps?(other)  # => true or false
+  #
+  # @param [Range] other
+  #   the other Range to compare with
+  #
+  # @return [Boolean]
+  #
+  # @api public
   def overlaps?(other)
     include?(other.first) || other.include?(first)
   end unless method_defined? :overlaps?
