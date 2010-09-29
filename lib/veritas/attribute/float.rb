aa3efecc0b2a0785c52cade10fc02b0845e3f931
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 22:01:59 -0700

    Added YARD docs for Attribute::Float

--- lib/veritas/attribute/float.rb
+++ lib/veritas/attribute/float.rb
@@ -1,6 +1,17 @@
 module Veritas
   class Attribute
+
+    # Represents a Float value in a relation tuple
     class Float < Numeric
+
+      # The Float primitive
+      #
+      # @example
+      #   Float.primitive  # => ::Float
+      #
+      # @return [::Float]
+      #
+      # @api public
       def self.primitive
         ::Float
       end
