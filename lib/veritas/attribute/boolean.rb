08711199d75af06f027ca0009d5ba5c40b975a20
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 21:38:41 -0700

    Added YARD docs for Attribute::Boolean

--- lib/veritas/attribute/boolean.rb
+++ lib/veritas/attribute/boolean.rb
@@ -1,12 +1,33 @@
 module Veritas
   class Attribute
+
+    # Represents a Boolean value in a relation tuple
     class Boolean < Object
       include Logic::Connective::Methods
 
+      # The Boolean primitive
+      #
+      # @example
+      #   Boolean.primitive  # => TrueClass
+      #
+      # @return [TrueClass]
+      #
+      # @api public
       def self.primitive
         TrueClass
       end
 
+      # Test if the value is a boolean
+      #
+      # @example
+      #   boolean.valid_value?(value)  # => true or false
+      #
+      # @param [Object] value
+      #   the value to test
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def valid_value?(value)
         valid_or_optional?(value) { super || value.kind_of?(FalseClass) }
       end
