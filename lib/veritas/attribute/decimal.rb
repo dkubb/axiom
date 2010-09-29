08ca8898ab7a7c4daa51a990fb7ce8816e3c4927
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 22:01:18 -0700

    Added YARD docs for Attribute::Decimal

--- lib/veritas/attribute/decimal.rb
+++ lib/veritas/attribute/decimal.rb
@@ -1,6 +1,17 @@
 module Veritas
   class Attribute
+
+    # Represents a Decimal value in a relation tuple
     class Decimal < Numeric
+
+      # The Decimal primitive
+      #
+      # @example
+      #   Decimal.primitive  # => BigDecimal
+      #
+      # @return [BigDecimal]
+      #
+      # @api public
       def self.primitive
         BigDecimal
       end
