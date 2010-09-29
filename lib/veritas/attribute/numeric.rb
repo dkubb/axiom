e6feb364b5452f82f518f84ed75ffad77559409e
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 22:15:02 -0700

    Added YARD docs for Attribute::Numeric

--- lib/veritas/attribute/numeric.rb
+++ lib/veritas/attribute/numeric.rb
@@ -1,5 +1,7 @@
 module Veritas
   class Attribute
+
+    # Represents a Numeric value in a relation tuple
     class Numeric < Object
       extend Aliasable
       include Comparable
@@ -8,21 +10,73 @@ module Veritas
 
       inheritable_alias(:range => :size)
 
+      # The Numeric range for a valid value
+      #
+      # @example
+      #   numeric.size  # => 0..10
+      #
+      # @return [Range<::Numeric>]
+      #
+      # @api public
       attr_reader :size
 
+      # The Numeric primitive
+      #
+      # @example
+      #   Numeric.primitive  # => ::Numeric
+      #
+      # @return [::Numeric]
+      #
+      # @api public
       def self.primitive
         ::Numeric
       end
 
+      # Initialize a Numeric Attribute
+      #
+      # @param [#to_sym] name
+      #   the attribute name
+      # @param [#to_hash] options
+      #   the options for the attribute
+      # @option options [Boolean] :required (true)
+      #   if true, then the value cannot be nil
+      # @option options [Range<::Numeric>] :size (0..2**31-1)
+      #   The numeric range for a valid value
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(*)
         super
         @size = @options.fetch(:size, DEFAULT_SIZE).to_inclusive
       end
 
+      # Test if the value matches the attribute constraints
+      #
+      # @example
+      #   numeric.valid_value?(value)  # => true or false
+      #
+      # @param [Object] value
+      #   the value to test
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def valid_value?(value)
         valid_or_optional?(value) { super && size.include?(value) }
       end
 
+      # Test if the attribute can be joined with the other attribute
+      #
+      # @example
+      #   numeric.joinable?(other)  # => true or false
+      #
+      # @param [Attribute] other
+      #   the other attribute to test
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def joinable?(other)
         super && size.eql?(other.size)
       end
