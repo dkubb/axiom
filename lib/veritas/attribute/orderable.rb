c48f0de9121044ee8ac86077e73a46e7ffb6d69e
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 22:17:42 -0700

    Added YARD docs for Attribute::Orderable

--- lib/veritas/attribute/orderable.rb
+++ lib/veritas/attribute/orderable.rb
@@ -1,10 +1,29 @@
 module Veritas
   class Attribute
+
+    # A mixin for attributes that have orderable values
     module Orderable
+
+      # Sort the attribute in ascending order
+      #
+      # @example
+      #   ascending = attribute.asc
+      #
+      # @return [Relation::Operation::Order::Ascending]
+      #
+      # @api public
       def asc
         Relation::Operation::Order::Ascending.new(self)
       end
 
+      # Sort the attribute in descending order
+      #
+      # @example
+      #   descending = attribute.desc
+      #
+      # @return [Relation::Operation::Order::Descending]
+      #
+      # @api public
       def desc
         Relation::Operation::Order::Descending.new(self)
       end
