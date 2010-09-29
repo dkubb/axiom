9671d79d4551895cdc50cd4c8dda050d2c0a59af
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 22:20:47 -0700

    Added YARD docs for Attribute::String

--- lib/veritas/attribute/string.rb
+++ lib/veritas/attribute/string.rb
@@ -1,5 +1,7 @@
 module Veritas
   class Attribute
+
+    # Represents a String value in a relation tuple
     class String < Object
       extend Aliasable
       include Orderable,
@@ -10,21 +12,73 @@ module Veritas
 
       inheritable_alias(:range => :length)
 
+      # The String length for a valid value
+      #
+      # @example
+      #   string.length  # => 0..50
+      #
+      # @return [Range<::Integer>]
+      #
+      # @api public
       attr_reader :length
 
+      # The String primitive
+      #
+      # @example
+      #   String.primitive  # => ::String
+      #
+      # @return [::String]
+      #
+      # @api public
       def self.primitive
         ::String
       end
 
+      # Initialize a String Attribute
+      #
+      # @param [#to_sym] name
+      #   the attribute name
+      # @param [#to_hash] options
+      #   the options for the attribute
+      # @option options [Boolean] :required (true)
+      #   if true, then the value cannot be nil
+      # @option options [Range<::Integer>] :length (0..50)
+      #   The string length for a valid value
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(*)
         super
         @length = @options.fetch(:length, DEFAULT_LENGTH).to_inclusive
       end
 
+      # Test if the value matches the attribute constraints
+      #
+      # @example
+      #   string.valid_value?(value)  # => true or false
+      #
+      # @param [Object] value
+      #   the value to test
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def valid_value?(value)
         valid_or_optional?(value) { super && length.include?(value.length) }
       end
 
+      # Test if the attribute can be joined with the other attribute
+      #
+      # @example
+      #   string.joinable?(other)  # => true or false
+      #
+      # @param [Attribute] other
+      #   the other attribute to test
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def joinable?(other)
         super && length.eql?(other.length)
       end
