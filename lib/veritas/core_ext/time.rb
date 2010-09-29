bb023de4ef5dda4d3b4e1db9965095306a6acfd1
Dan Kubb <dan.kubb@gmail.com>
2010-09-29 08:58:26 -0700

    Added YARD docs for Time

--- lib/veritas/core_ext/time.rb
+++ lib/veritas/core_ext/time.rb
@@ -1,4 +1,14 @@
+# Extend Time with methods to find the previous Time
 class Time
+
+  # Decrement the Time by one second
+  #
+  # @example
+  #   previous_second = time.pred
+  #
+  # @return [Time]
+  #
+  # @api public
   def pred
     self - 1
   end unless method_defined? :pred
