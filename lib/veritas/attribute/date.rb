14e498501c91cbf216c213449a28e4d31db77169
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 21:55:03 -0700

    Added YARD docs for Attribute::Date

--- lib/veritas/attribute/date.rb
+++ lib/veritas/attribute/date.rb
@@ -1,14 +1,32 @@
 module Veritas
   class Attribute
+
+    # Represents a Date value in a relation tuple
     class Date < Object
       include Comparable
 
       DEFAULT_RANGE = (::Date.new..::Date::Infinity.new).freeze
 
+      # The Date primitive
+      #
+      # @example
+      #   Date.primitive  # => ::Date
+      #
+      # @return [::Date]
+      #
+      # @api public
       def self.primitive
         ::Date
       end
 
+      # The Date range for a valid value
+      #
+      # @example
+      #   Date.range  # => ::Date.new(*from)..::Date.new(*to)
+      #
+      # @return [Range<::Date>]
+      #
+      # @api public
       def range
         DEFAULT_RANGE
       end
