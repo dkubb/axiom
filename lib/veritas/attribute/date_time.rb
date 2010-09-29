995f9497074367c3937f04bebccddb1b501ce57e
Dan Kubb <dan.kubb@gmail.com>
2010-09-28 21:59:12 -0700

    Added YARD docs for Attribute::DateTime

--- lib/veritas/attribute/date_time.rb
+++ lib/veritas/attribute/date_time.rb
@@ -1,14 +1,32 @@
 module Veritas
   class Attribute
+
+    # Represents a DateTime value in a relation tuple
     class DateTime < Object
       include Comparable
 
       DEFAULT_RANGE = (::DateTime.new..::DateTime::Infinity.new).freeze
 
+      # The DateTime primitive
+      #
+      # @example
+      #   DateTime.primitive  # => ::DateTime
+      #
+      # @return [::DateTime]
+      #
+      # @api public
       def self.primitive
         ::DateTime
       end
 
+      # The DateTime range for a valid value
+      #
+      # @example
+      #   DateTime.range  # => ::DateTime.new(*from)..::DateTime.new(*to)
+      #
+      # @return [Range<::DateTime>]
+      #
+      # @api public
       def range
         DEFAULT_RANGE
       end
