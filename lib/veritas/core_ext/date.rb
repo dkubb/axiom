56d09a608ed9e1d3baba695daf2735c1bfc8eaf6
Dan Kubb <dan.kubb@gmail.com>
2010-09-29 08:51:25 -0700

    Added YARD docs for Date

--- lib/veritas/core_ext/date.rb
+++ lib/veritas/core_ext/date.rb
@@ -1,4 +1,14 @@
+# Extend Date with methods to find the previous Date
 class Date
+
+  # Decrement the Date by one day
+  #
+  # @example
+  #   previous_day = date.pred
+  #
+  # @return [Date]
+  #
+  # @api public
   def pred
     self - 1
   end unless method_defined? :pred
