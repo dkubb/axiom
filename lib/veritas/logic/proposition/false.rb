9e1edb71f52f5059e746641f302c8bbaed46f73b
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 16:04:44 -0700

    Added YARD docs for False proposition

--- lib/veritas/logic/proposition/false.rb
+++ lib/veritas/logic/proposition/false.rb
@@ -2,18 +2,55 @@ module Veritas
   module Logic
     class Proposition
       class False < Proposition
+
+        # Return the inverse proposition class
+        #
+        # @example
+        #   False.inverse  # => True
+        #
+        # @return [Class<True>]
+        #
+        # @api public
         def self.inverse
           True
         end
 
+        # Evaluate the proposition
+        #
+        # @example
+        #   False.call  # => false
+        #
+        # @return [false]
+        #
+        # @api public
         def self.call
           false
         end
 
+        # Logically AND the proposition with another expression
+        #
+        # @example
+        #   false_proposition.and(other)  # => false_proposition
+        #
+        # @param [Expression] other
+        #
+        # @return [self]
+        #
+        # @api public
         def and(other)
           self
         end
 
+        # Logically OR the proposition with another expression
+        #
+        # @example
+        #   false_proposition.or(other)  # => other
+        #
+        # @param [Expression] other
+        #
+        # @return [Expression]
+        #
+        # @api public
         def or(other)
           other
         end
