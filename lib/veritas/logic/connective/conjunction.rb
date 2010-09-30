e2edfb502369e3c3092028badd3936a2ca81ae80
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 12:35:55 -0700

    Added YARD docs for Conjunction

--- lib/veritas/logic/connective/conjunction.rb
+++ lib/veritas/logic/connective/conjunction.rb
@@ -1,11 +1,42 @@
 module Veritas
   module Logic
     class Connective
+
+      # A logical AND between expressions
       class Conjunction < Binary
+
+        # Evaluate the operands using a logical AND
+        #
+        # @example with true operands
+        #   Conjunction.call(true, true)  # => true
+        #
+        # @example with true and false
+        #   Conjunction.call(true, false)  # => false
+        #
+        # @example with false and true
+        #   Conjunction.call(false, true)  # => false
+        #
+        # @example with false and false
+        #   Conjunction.call(false, false)  # => false
+        #
+        # @param [Boolean] left
+        # @param [Boolean] right
+        #
+        # @return [Boolean]
+        #
+        # @api public
         def self.call(left, right)
           left && right
         end
 
+        # Return a string representing the conjunction
+        #
+        # @example
+        #   conjunction.inspect  # "<Expression1> AND <Expression2>"
+        #
+        # @return [String]
+        #
+        # @api public
         def inspect
           "(#{left.inspect} AND #{right.inspect})"
         end
@@ -15,6 +46,16 @@ module Veritas
 
           inheritable_alias(:& => :and)
 
+          # Logically AND the expression with another expression
+          #
+          # @example
+          #   conjunction = expression.and(other)
+          #
+          # @param [Expression] other
+          #
+          # @return [Conjunction]
+          #
+          # @api public
           def and(other)
             Conjunction.new(self, other)
           end
