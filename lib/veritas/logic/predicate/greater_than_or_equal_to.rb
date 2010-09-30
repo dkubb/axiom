89a0a429d9152786ce906beec3a8ab6d2b90135d
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 15:39:00 -0700

    Added YARD docs for GreaterThanOrEqualTo

--- lib/veritas/logic/predicate/greater_than_or_equal_to.rb
+++ lib/veritas/logic/predicate/greater_than_or_equal_to.rb
@@ -1,22 +1,59 @@
 module Veritas
   module Logic
     class Predicate
+
+      # A predicate representing a greater than or equal to test between operands
       class GreaterThanOrEqualTo < Predicate
         include Comparable
 
+        # Return the GreaterThanOrEqualTo operation
+        #
+        # @example
+        #   GreaterThanOrEqualTo.operation  # => :>=
+        #
+        # @return [Symbol]
+        #
+        # @api public
         def self.operation
           :>=
         end
 
+        # Return the inverse predicate class
+        #
+        # @example
+        #   GreaterThanOrEqualTo.inverse  # => LessThan
+        #
+        # @return [Class<LessThan>]
+        #
+        # @api public
         def self.inverse
           LessThan
         end
 
+        # Return the reverse predicate class
+        #
+        # @example
+        #   GreaterThanOrEqualTo.reverse  # => LessThanOrEqualTo
+        #
+        # @return [Class<LessThanOrEqualTo>]
+        #
+        # @api public
         def self.reverse
           LessThanOrEqualTo
         end
 
         module Methods
+
+          # Compare the left to see if it is greater than or equal to the right
+          #
+          # @example
+          #   greater_than_or_equal_to = expression.gte(other)
+          #
+          # @param [Expression] other
+          #
+          # @return [GreaterThanOrEqualTo]
+          #
+          # @api public
           def gte(other)
             GreaterThanOrEqualTo.new(self, other)
           end
