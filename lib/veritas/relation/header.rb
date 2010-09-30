23d8400e1d6695864f0512a1830816f28cf40aa3
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 00:32:34 -0700

    Added YARD docs for Relation::Header

--- lib/veritas/relation/header.rb
+++ lib/veritas/relation/header.rb
@@ -10,6 +10,17 @@ module Veritas
         :- => :difference
       )
 
+      # Initialize a Header
+      #
+      # @example
+      #   header = Header.new(attributes)
+      #
+      # @param [#to_ary]
+      #   optional attributes
+      #
+      # @return [undefined]
+      #
+      # @api public
       def initialize(attributes = [])
         @attributes = attributes.to_ary.map do |attribute|
           Attribute.coerce(attribute)
@@ -18,11 +29,38 @@ module Veritas
         @indexes = {}
       end
 
+      # Iterate over each attribute in the header
+      #
+      # @example
+      #   header = Header.new(attributes)
+      #   header.each { |attribute| ... }
+      #
+      # @yield [attribute]
+      #
+      # @yieldparam [Attribute] attribute
+      #   each attribute in the header
+      #
+      # @return [self]
+      #
+      # @api public
       def each(&block)
         to_ary.each(&block)
         self
       end
 
+      # Lookup the index of an attribute in the header given a name
+      #
+      # @example
+      #   index = header.index(:id)
+      #
+      # @param [Attribute, #to_ary, #to_sym] name
+      #
+      # @return [Integer]
+      #   the offset when the name is known
+      # @return [nil]
+      #   nil when the attribute is unknown
+      #
+      # @api private
       def index(name)
         @indexes[name] ||=
           begin
@@ -31,6 +69,19 @@ module Veritas
           end
       end
 
+      # Lookup an attribute in the header given a name
+      #
+      # @example
+      #   attribute = header[:id]
+      #
+      # @param [Attribute, #to_ary, #to_sym] name
+      #
+      # @return [Attribute]
+      #   the attribute when the name is known
+      # @return [nil]
+      #   nil when the attribute is unknown
+      #
+      # @api public
       def [](name)
         @names[name] ||=
           begin
@@ -39,57 +90,173 @@ module Veritas
           end
       end
 
+      # Return a header with only the attributes specified
+      #
+      # @example
+      #   projected = header.project(attributes)
+      #
+      # @param [#map] attributes
+      #   the attributes to keep in the header
+      #
+      # @return [Header]
+      #
+      # @api private
       def project(attributes)
         new(attributes.map { |attribute| self[attribute] })
       end
 
+      # Return a header with the attributes renamed
+      #
+      # @example
+      #   renamed = header.rename(aliases)
+      #
+      # @param [Aliases] aliases
+      #   the old and new attribute names
+      #
+      # @return [Header]
+      #
+      # @api private
       def rename(aliases)
         new(map { |attribute| aliases[attribute] })
       end
 
+      # Return the intersection of the header with another header
+      #
+      # @param [Header] other
+      #
+      # @return [Header]
+      #
+      # @api private
       def intersect(other)
         new(to_ary & other)
       end
 
+      # Return the union of the header with another header
+      #
+      # @param [Header] other
+      #
+      # @return [Header]
+      #
+      # @api private
       def union(other)
         new(to_ary | other)
       end
 
+      # Return the difference of the header with another header
+      #
+      # @param [Header] other
+      #
+      # @return [Header]
+      #
+      # @api private
       def difference(other)
         new(to_ary - other)
       end
 
+      # Convert the Header into an Array
+      #
+      # @example
+      #   array = header.to_ary
+      #
+      # @return [Array]
+      #
+      # @api private
       def to_ary
         @attributes
       end
 
+      # Compare the header with other header for equivalency
+      #
+      # @example
+      #   header == other  # => true or false
+      #
+      # @param [Header] other
+      #   the other header to compare with
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def ==(other)
         to_set == self.class.coerce(other).to_set
       end
 
+      # Compare the header with other header for equality
+      #
+      # @example
+      #   header.eql?(other)  # => true or false
+      #
+      # @param [Header] other
+      #   the other header to compare with
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def eql?(other)
         instance_of?(other.class) &&
         to_set == other.to_set
       end
 
+      # Return the hash of the header
+      #
+      # @example
+      #   numeric_hash = header.hash
+      #
+      # @return [Integer]
+      #
+      # @api public
       def hash
         to_ary.hash
       end
 
+      # Test if there are no attributes
+      #
+      # @example
+      #   header.empty?  # => true or false
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def empty?
         to_ary.empty?
       end
 
+      # Return a string representing the header
+      #
+      # @example
+      #   header.inspect  # => "[<Attribute::Integer name: id>]"
+      #
+      # @return [String]
+      #
+      # @api public
       def inspect
         to_ary.inspect
       end
 
     private
 
+      # Utility method to instantiate a Header
+      #
+      # @param [#to_ary] attributes
+      #   the header attributes
+      #
+      # @return [Header]
+      #
+      # @api private
       def new(attributes)
         self.class.new(attributes)
       end
 
+      # Coerce an Array-like object into a Header
+      #
+      # @example
+      #   header = Header.coerce([ [ :id, Integer ] ])
+      #
+      # @param [Header, #to_ary]
+      #   the header or attributes
+      #
+      # @return [Header]
+      #
+      # @api private
       def self.coerce(object)
         object.kind_of?(Header) ? object : new(object)
       end
