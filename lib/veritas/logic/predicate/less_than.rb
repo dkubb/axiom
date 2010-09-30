6a1d1dc5837f6a6da94c18f52f5c1f9fb375f788
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 15:43:53 -0700

    Added YARD docs for LessThan

--- lib/veritas/logic/predicate/less_than.rb
+++ lib/veritas/logic/predicate/less_than.rb
@@ -1,22 +1,59 @@
 module Veritas
   module Logic
     class Predicate
+
+      # A predicate representing a less than test between operands
       class LessThan < Predicate
         include Comparable
 
+        # Return the LessThan operation
+        #
+        # @example
+        #   LessThan.operation  # => :<
+        #
+        # @return [Symbol]
+        #
+        # @api public
         def self.operation
           :<
         end
 
+        # Return the inverse predicate class
+        #
+        # @example
+        #   LessThan.inverse  # => GreaterThanOrEqualTo
+        #
+        # @return [Class<GreaterThanOrEqualTo>]
+        #
+        # @api public
         def self.inverse
           GreaterThanOrEqualTo
         end
 
+        # Return the reverse predicate class
+        #
+        # @example
+        #   LessThan.reverse  # => GreaterThan
+        #
+        # @return [Class<GreaterThan>]
+        #
+        # @api public
         def self.reverse
           GreaterThan
         end
 
         module Methods
+
+          # Compare the left to see if it is less than the right
+          #
+          # @example
+          #   less_than = expression.lt(other)
+          #
+          # @param [Expression] other
+          #
+          # @return [LessThan]
+          #
+          # @api public
           def lt(other)
             LessThan.new(self, other)
           end
