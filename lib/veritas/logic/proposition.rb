a22a31f65089971fe7490893658e27bd7564968d
Dan Kubb <dan.kubb@gmail.com>
2010-09-29 23:55:52 -0700

    Added YARD docs for Proposition

--- lib/veritas/logic/proposition.rb
+++ lib/veritas/logic/proposition.rb
@@ -3,6 +3,31 @@ module Veritas
     class Proposition < Expression
       include Singleton
 
+      # Instantiate a new Proposition
+      #
+      # @example using a true value
+      #   true_proposition = Proposition.new(true)
+      #
+      # @example using a false value
+      #   false_proposition = Proposition.new(false)
+      #
+      # @return [Proposition]
+      #
+      # @overload self.new(true)
+      #   Return the True singleton instance
+      #
+      #   @param [Array(true)] *args
+      #
+      #   @return [True]
+      #
+      # @overload self.new(false)
+      #   Return the False singleton instance
+      #
+      #   @param [Array(false)] *args
+      #
+      #   @return [False]
+      #
+      # @api public
       def self.new(*args)
         if args.empty?
           super
@@ -11,22 +36,64 @@ module Veritas
         end
       end
 
+      # Evaluate the proposition
+      #
+      # @example
+      #   proposition.call  # => true or false
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def call(*)
         self.class.call
       end
 
+      # Return the inverse proposition
+      #
+      # @example
+      #   inverse = proposition.inverse
+      #
+      # @return [Proposition]
+      #
+      # @api public
       def inverse
         self.class.inverse.instance
       end
 
+      # Compare the proposition with other proposition for equality
+      #
+      # @example
+      #   proposition.eql?(other)  # => true or false
+      #
+      # @param [Proposition] other
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def eql?(other)
         instance_of?(other.class)
       end
 
+      # Return the hash of the proposition
+      #
+      # @example
+      #   numeric_hash = proposition.hash
+      #
+      # @return [Integer]
+      #
+      # @api public
       def hash
         call.hash
       end
 
+      # Return a string representing the proposition
+      #
+      # @example
+      #   proposition.inspect  # (String representation of Proposition)
+      #
+      # @return [String]
+      #
+      # @api public
       def inspect
         call.inspect
       end
