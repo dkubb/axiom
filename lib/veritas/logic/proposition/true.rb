b85e2d4ba375cc2a3c83a3044e4b66f89d435f6c
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 16:07:45 -0700

    Added YARD docs for True proposition

--- lib/veritas/logic/proposition/true.rb
+++ lib/veritas/logic/proposition/true.rb
@@ -2,18 +2,55 @@ module Veritas
   module Logic
     class Proposition
       class True < Proposition
+
+        # Return the inverse proposition class
+        #
+        # @example
+        #   True.inverse  # => False
+        #
+        # @return [Class<False>]
+        #
+        # @api public
         def self.inverse
           False
         end
 
+        # Evaluate the proposition
+        #
+        # @example
+        #   True.call  # => true
+        #
+        # @return [true]
+        #
+        # @api public
         def self.call
           true
         end
 
+        # Logically AND the proposition with another expression
+        #
+        # @example
+        #   true_proposition.and(other)  # => other
+        #
+        # @param [Expression] other
+        #
+        # @return [Expression]
+        #
+        # @api public
         def and(other)
           other
         end
 
+        # Logically OR the proposition with another expression
+        #
+        # @example
+        #   true_proposition.or(other)  # => true_proposition
+        #
+        # @param [Expression] other
+        #
+        # @return [self]
+        #
+        # @api public
         def or(other)
           self
         end
