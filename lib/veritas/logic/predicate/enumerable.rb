81177282eba7732ae121ac788c2103985bc022a3
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 14:28:18 -0700

    Added YARD docs for Predicate::Enumerable

--- lib/veritas/logic/predicate/enumerable.rb
+++ lib/veritas/logic/predicate/enumerable.rb
@@ -1,8 +1,20 @@
 module Veritas
   module Logic
     class Predicate
+
+      # A mixin for predicates matching an enumerable
       module Enumerable
 
+        # Initialize an Enumerable predicate
+        #
+        # @param [Object] left
+        #   the attribute or value to test for in the Enumerable
+        # @param [Enumerable] right
+        #   the enumerable to test
+        #
+        # @return [undefined]
+        #
+        # @api private
         def initialize(left, right)
           super(left, Immutable.freeze_object(right))
         end
