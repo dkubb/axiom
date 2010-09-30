ed3aa975ba8d28aa83b04a1cc67496766342fbfe
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 12:14:28 -0700

    Added YARD docs for Connective::Binary

--- lib/veritas/logic/connective/binary.rb
+++ lib/veritas/logic/connective/binary.rb
@@ -1,13 +1,40 @@
 module Veritas
   module Logic
     class Connective
+
+      # A mixin for connectives that have two operands
       module Binary
         include Operation::Binary
 
+        # Evaluate the binary connective using the tuple
+        #
+        # @example
+        #   binary.call(tuple)  # => true or false
+        #
+        # @param [Tuple] tuple
+        #   the tuple to pass to #call in the left and right operands
+        #
+        # @return [Boolean]
+        #
+        # @api public
         def call(tuple)
           self.class.call(left.call(tuple), right.call(tuple))
         end
 
+        # Rename the contained attributes with the provided aliases
+        #
+        # @example
+        #   renamed = binary.rename(aliases)
+        #
+        # @param [Algebra::Rename::Aliases] aliases
+        #   the old and new attributes
+        #
+        # @return [self]
+        #   if the left and right operands are not renamed
+        # @return [Binary]
+        #   if the left or right operand is renamed
+        #
+        # @api public
         def rename(aliases)
           left          = self.left
           right         = self.right
@@ -21,6 +48,14 @@ module Veritas
           end
         end
 
+        # Return the inverse connective
+        #
+        # @example
+        #   negation = binary.inverse
+        #
+        # @return [Negation]
+        #
+        # @api public
         def inverse
           Negation.new(self)
         end
