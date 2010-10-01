aee53e50cfa7c0682027ca2a39ca4ba766bde39d
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 21:28:20 -0700

    Added YARD docs for Relation::Operation::Binary

--- lib/veritas/relation/operation/binary.rb
+++ lib/veritas/relation/operation/binary.rb
@@ -1,14 +1,32 @@
 module Veritas
   class Relation
     module Operation
+
+      # A mixin for Binary relations
       module Binary
         include Veritas::Operation::Binary
 
+        # Hook called when module is included
+        #
+        # @param [Module] descendant
+        #   the module or class including Binary
+        #
+        # @return [self]
+        #
+        # @api private
         def self.included(descendant)
           descendant.extend ClassMethods
           self
         end
 
+        # Initialize a Binary relation
+        #
+        # @param [Relation] left
+        # @param [Relation] right
+        #
+        # @return [undefined]
+        #
+        # @api private
         def initialize(left, right)
           super
           @header     = left.header     | right.header
@@ -16,6 +34,18 @@ module Veritas
         end
 
         module ClassMethods
+
+          # Instantiate a new Binary relation
+          #
+          # @example
+          #   binary = BinaryRelation.new(left, right)
+          #
+          # @param [Relation] left
+          # @param [Relation] right
+          #
+          # @return [Binary]
+          #
+          # @api public
           def new(left, right)
             assert_ordered_match(left, right)
             super
@@ -23,6 +53,14 @@ module Veritas
 
         private
 
+          # Assert that ordered and unordered relations are not mixed
+          #
+          # @return [undefined]
+          #
+          # @raise [RelationMismatchError]
+          #   raised if one relation is ordered and the other is not
+          #
+          # @api private
           def assert_ordered_match(left, right)
             if left.directions.empty? != right.directions.empty?
               raise RelationMismatchError, 'both relations must be ordered or neither may be ordered'
