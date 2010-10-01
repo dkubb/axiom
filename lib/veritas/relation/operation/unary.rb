e6096e7c5a82532d02ff1a984acf1ddfe0c83be2
Dan Kubb <dan.kubb@gmail.com>
2010-09-30 21:29:31 -0700

    Added YARD docs for Relation::Operation::Unary

--- lib/veritas/relation/operation/unary.rb
+++ lib/veritas/relation/operation/unary.rb
@@ -1,9 +1,18 @@
 module Veritas
   class Relation
     module Operation
+
+      # A mixin for Unary relations
       module Unary
         include Veritas::Operation::Unary
 
+        # Initialize a Unary relation
+        #
+        # @param [Relation] operand
+        #
+        # @return [undefined]
+        #
+        # @api private
         def initialize(operand)
           super
           @header     = operand.header
