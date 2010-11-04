7d7a144856999aadd9028138287aefc7de1ef1e8
Dan Kubb <dan.kubb@gmail.com>
2010-11-03 20:36:28 -0700

    Added YARD docs for Relation::Operation::Set

--- lib/veritas/relation/operation/set.rb
+++ lib/veritas/relation/operation/set.rb
@@ -4,17 +4,45 @@ module Veritas
       module Set
         include Binary
 
+        # Hook called when module is included
+        #
+        # @param [Module] descendant
+        #   the module or class including Set
+        #
+        # @return [self]
+        #
+        # @api private
         def self.included(descendant)
           descendant.extend ClassMethods
           self
         end
 
+        # Initialize a Set
+        #
+        # @param [Relation] left
+        # @param [Relation] right
+        #
+        # @return [undefined]
+        #
+        # @api private
         def initialize(left, right)
           super
           @header = left.header
         end
 
         module ClassMethods
+
+          # Instantiate a new Set relation
+          #
+          # @example
+          #   set = SetRelation.new(left, right)
+          #
+          # @param [Relation] left
+          # @param [Relation] right
+          #
+          # @return [Set]
+          #
+          # @api public
           def new(left, right)
             assert_equivalent_headers(left, right)
             super
@@ -22,6 +50,17 @@ module Veritas
 
         private
 
+          # Assert that left and right have equivalent headers
+          #
+          # @param [Relation] left
+          # @param [Relation] right
+          #
+          # @return [undefined]
+          #
+          # @raise [InvalidHeaderError]
+          #   raised if the headers are not equivalent
+          #
+          # @api private
           def assert_equivalent_headers(left, right)
             if left.header != right.header
               raise InvalidHeaderError, "the headers must be equivalent for #{name}.new"
