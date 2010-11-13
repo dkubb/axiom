f3ee40d05a86bba3b75dbed2d20a306b551959a6
Dan Kubb <dan.kubb@gmail.com>
2010-11-12 21:22:16 -0800

    Added YARD docs for Optimizer::Algebra::Difference

--- lib/veritas/optimizer/algebra/difference.rb
+++ lib/veritas/optimizer/algebra/difference.rb
@@ -1,28 +1,49 @@
 module Veritas
   class Optimizer
     module Algebra
+
+      # Abstract base class representing Difference optimizations
       class Difference < Relation::Operation::Binary
+
+        # Optimize when operands are equal in a Difference
         class EqualOperands < self
           include Relation::Operation::Binary::EqualOperands
 
+          # A Difference with equal operands is empty
+          #
+          # @return [Relation::Empty]
+          #
+          # @api private
           def optimize
             Veritas::Relation::Empty.new(operation.header)
           end
 
         end # class EqualOperands
 
+        # Optimize when the left operand is empty in a Difference
         class EmptyLeft < self
           include Relation::Operation::Binary::EmptyLeft
 
+          # A Difference with an empty left operand is empty
+          #
+          # @return [Relation::Empty]
+          #
+          # @api private
           def optimize
             left
           end
 
         end # class EmptyLeft
 
+        # Optimize when the right operand is empty in a Difference
         class EmptyRight < self
           include Relation::Operation::Binary::EmptyRight
 
+          # A Difference with an empty right operand is equivalent to the left
+          #
+          # @return [Relation]
+          #
+          # @api private
           def optimize
             left
           end
