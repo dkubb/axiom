ee64ff09b70456d0e4e9b1f26468cd18ba089e8c
Dan Kubb <dan.kubb@gmail.com>
2010-11-12 21:27:25 -0800

    Added YARD docs for Optimizer::Algebra::Extension

--- lib/veritas/optimizer/algebra/extension.rb
+++ lib/veritas/optimizer/algebra/extension.rb
@@ -1,12 +1,27 @@
 module Veritas
   class Optimizer
     module Algebra
+
+      # Abstract base class representing Extension optimizations
       class Extension < Relation::Operation::Unary
+
+        # Optimize when operands are optimizable
         class UnoptimizedOperand < self
+
+          # Test if the Extension has an unoptimized operand
+          #
+          # @return [Boolean]
+          #
+          # @api private
           def optimizable?
             !operand.equal?(operation.operand)
           end
 
+          # Return an Extension with optimized operands
+          #
+          # @return [Algebra::Extension]
+          #
+          # @api private
           def optimize
             operation = self.operation
             operation.class.new(operand, operation.extensions)
