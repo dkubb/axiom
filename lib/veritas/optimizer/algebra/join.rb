6c827173ac21d6de2633ff65cee916779d5e55e3
Dan Kubb <dan.kubb@gmail.com>
2010-11-12 21:40:05 -0800

    Added YARD docs for Optimizer::Algebra::Join

--- lib/veritas/optimizer/algebra/join.rb
+++ lib/veritas/optimizer/algebra/join.rb
@@ -1,17 +1,32 @@
 module Veritas
   class Optimizer
     module Algebra
+
+      # Abstract base class representing Join optimizations
       class Join < Relation::Operation::Combine
+
+        # Optimize when operand headers are equivalent
         class EqualHeaders < self
+
+          # Test if the operands' headers are equal
+          #
+          # @return [Boolean]
+          #
+          # @api private
           def optimizable?
             left.header.eql?(right.header)
           end
 
+          # A Join with equal headers is an Intersection
+          #
+          # @return [Algebra::Intersection]
+          #
+          # @api private
           def optimize
             Veritas::Algebra::Intersection.new(left, right)
           end
 
-        end
+        end # class EqualHeaders
 
         Veritas::Algebra::Join.optimizer = chain(
           EmptyLeft,
