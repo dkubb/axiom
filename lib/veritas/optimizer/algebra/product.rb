2e4bba1705ec7a6c0992978b1645b1d433f928d8
Dan Kubb <dan.kubb@gmail.com>
2010-11-12 21:44:58 -0800

    Added YARD docs for Optimizer::Algebra::Product

--- lib/veritas/optimizer/algebra/product.rb
+++ lib/veritas/optimizer/algebra/product.rb
@@ -1,25 +1,52 @@
 module Veritas
   class Optimizer
     module Algebra
+
+      # Abstract base class representing Product optimizations
       class Product < Relation::Operation::Combine
+
+        # Optimize when left operand is a TABLE DEE
         class TableDeeLeft < self
+
+          # Test if the left operand is a TABLE DEE
+          #
+          # @return [Boolean]
+          #
+          # @api private
           def optimizable?
             left = self.left
             left.header.empty? && !left.kind_of?(Veritas::Relation::Empty)
           end
 
+          # A Product wih a left TABLE DEE is equivalent to the right operand
+          #
+          # @return [Relation]
+          #
+          # @api private
           def optimize
             right
           end
 
         end # class TableDeeLeft
 
+        # Optimize when right operand is a TABLE DEE
         class TableDeeRight < self
+
+          # Test if the right operand is a TABLE DEE
+          #
+          # @return [Boolean]
+          #
+          # @api private
           def optimizable?
             right = self.right
             right.header.empty? && !right.kind_of?(Veritas::Relation::Empty)
           end
 
+          # A Product wih a right TABLE DEE is equivalent to the left operand
+          #
+          # @return [Relation]
+          #
+          # @api private
           def optimize
             left
           end
