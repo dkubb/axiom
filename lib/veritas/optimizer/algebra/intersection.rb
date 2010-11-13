73616a01acac0e5d704628e07ecc1905ca11a4c4
Dan Kubb <dan.kubb@gmail.com>
2010-11-12 21:32:33 -0800

    Added YARD docs for Optimizer::Algebra::Intersection

--- lib/veritas/optimizer/algebra/intersection.rb
+++ lib/veritas/optimizer/algebra/intersection.rb
@@ -1,19 +1,34 @@
 module Veritas
   class Optimizer
     module Algebra
+
+      # Abstract base class representing Intersection optimizations
       class Intersection < Relation::Operation::Binary
+
+        # Optimize when operands are equal in an Intersection
         class EqualOperands < self
           include Relation::Operation::Binary::EqualOperands
 
+          # An Intersection with equal operands is equivalent to either operand
+          #
+          # @return [Relation]
+          #
+          # @api private
           def optimize
             left
           end
 
         end # class EqualOperands
 
+        # Optimize when the left operand is empty in an EmptyLeft
         class EmptyLeft < self
           include Relation::Operation::Binary::EmptyLeft
 
+          # An Intersection with an empty left operand is empty
+          #
+          # @return [Relation::Empty]
+          #
+          # @api private
           def optimize
             left
           end
@@ -23,6 +38,11 @@ module Veritas
         class EmptyRight < self
           include Relation::Operation::Binary::EmptyRight
 
+          # An Intersection with an empty right operand is empty
+          #
+          # @return [Relation::Empty]
+          #
+          # @api private
           def optimize
             right
           end
