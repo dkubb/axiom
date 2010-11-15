65e8358fbb3ef140b25898aeb322eed25e4827ae
Dan Kubb <dan.kubb@gmail.com>
2010-11-14 19:44:34 -0800

    Added YARD docs for Optimizer::Relation::Materialized

--- lib/veritas/optimizer/relation/materialized.rb
+++ lib/veritas/optimizer/relation/materialized.rb
@@ -1,12 +1,27 @@
 module Veritas
   class Optimizer
     module Relation
+
+      # Abstract base class representing Materialized optimizations
       class Materialized < Optimizer
+
+        # Optimize when the operand is Empty
         class EmptyOperand < self
+
+          # Test if the operand is empty
+          #
+          # @return [Boolean]
+          #
+          # @api private
           def optimizable?
             operation.empty?
           end
 
+          # Return a new Empty relation with the operation's headers
+          #
+          # @return [Empty]
+          #
+          # @api private
           def optimize
             Veritas::Relation::Empty.new(operation.header)
           end
