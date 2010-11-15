f9ccea7cf793852fd721716b38a564f30fde2172
Dan Kubb <dan.kubb@gmail.com>
2010-11-14 20:39:34 -0800

    Added YARD docs for Optimizer::Relation::Operation::Reverse

--- lib/veritas/optimizer/relation/operation/reverse.rb
+++ lib/veritas/optimizer/relation/operation/reverse.rb
@@ -2,23 +2,50 @@ module Veritas
   class Optimizer
     module Relation
       module Operation
+
+        # Abstract base class representing Reverse optimizations
         class Reverse < Order
+
+          # Optimize when the operand is a Reverse
           class ReverseOperand < self
+
+            # Test if the operand is a Reverse
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               operand.kind_of?(operation.class)
             end
 
+            # A Reverse of a Reverse is a noop
+            #
+            # @return [Relation]
+            #
+            # @api private
             def optimize
               operand.operand
             end
 
           end # class ReverseOperand
 
+          # Optimize when the operand is an Order
           class OrderOperand < self
+
+            # Test if the operand is an Order
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               operand.kind_of?(Veritas::Relation::Operation::Order)
             end
 
+            # Flatten Reverse operation and Order operand into an Order
+            #
+            # @return [Order]
+            #
+            # @api private
             def optimize
               operand = self.operand
               operand.class.new(operand.operand, operation.directions)
@@ -26,11 +53,23 @@ module Veritas
 
           end # class OrderOperand
 
+          # Optimize when operand is optimizable
           class UnoptimizedOperand < self
+
+            # Test if the operand is unoptimized
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               !operand.equal?(operation.operand)
             end
 
+            # Return an Reverse with an optimized operand
+            #
+            # @return [Reverse]
+            #
+            # @api private
             def optimize
               operation.class.new(operand)
             end
