1e5ebac78ed1d3423a600422796a25139e4e2cb2
Dan Kubb <dan.kubb@gmail.com>
2010-11-14 19:04:04 -0800

    Added YARD docs for Optimizer::Algebra::Summarization

--- lib/veritas/optimizer/algebra/summarization.rb
+++ lib/veritas/optimizer/algebra/summarization.rb
@@ -1,12 +1,27 @@
 module Veritas
   class Optimizer
     module Algebra
+
+      # Abstract base class representing Summarization optimizations
       class Summarization < Relation::Operation::Unary
+
+        # Optimize when operand is optimizable
         class UnoptimizedOperand < self
+
+          # Test if the operand is unoptimized
+          #
+          # @return [Boolean]
+          #
+          # @api private
           def optimizable?
             !operand.equal?(operation.operand)
           end
 
+          # Return a Summarization with an optimized operand
+          #
+          # @return [Rename]
+          #
+          # @api private
           def optimize
             operation = self.operation
             operation.class.new(operand, operation.summarize_by, operation.summarizers)
