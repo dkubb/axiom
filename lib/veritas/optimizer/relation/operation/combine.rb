24679d0929b2f946a8e0c1737d38265b90587e93
Dan Kubb <dan.kubb@gmail.com>
2010-11-14 20:12:05 -0800

    Added YARD docs for Optimizer::Relation::Operation::Combine

--- lib/veritas/optimizer/relation/operation/combine.rb
+++ lib/veritas/optimizer/relation/operation/combine.rb
@@ -2,15 +2,25 @@ module Veritas
   class Optimizer
     module Relation
       module Operation
+
+        # Abstract base class representing combinations of relations
         class Combine < Binary
+
+          # Any relation combined with an empty relation is an empty relation
+          #
+          # @return [Relation::Empty]
+          #
+          # @api private
           def optimize
             Veritas::Relation::Empty.new(operation.header)
           end
 
+          # Optimize when the left operand is Empty
           class EmptyLeft < self
             include Binary::EmptyLeft
           end # class EmptyLeft
 
+          # Optimize when the right operand is Empty
           class EmptyRight < self
             include Binary::EmptyRight
           end # class EmptyRight
