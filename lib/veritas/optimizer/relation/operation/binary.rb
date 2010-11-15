068cc9c53290e33c42e9efd82a1a4a90c8f4c1c3
Dan Kubb <dan.kubb@gmail.com>
2010-11-14 20:00:34 -0800

    Added YARD docs for Optimizer::Relation::Operation::Binary

--- lib/veritas/optimizer/relation/operation/binary.rb
+++ lib/veritas/optimizer/relation/operation/binary.rb
@@ -2,9 +2,29 @@ module Veritas
   class Optimizer
     module Relation
       module Operation
-        class Binary < Optimizer
-          attr_reader :left, :right
 
+        # Abstract base class representing Binary relation optimizations
+        class Binary < Optimizer
+
+          # The optimized left operand
+          #
+          # @return [Relation]
+          #
+          # @api private
+          attr_reader :left
+
+          # The optimized right operand
+          #
+          # @return [Relation]
+          #
+          # @api private
+          attr_reader :right
+
+          # Initialize an Binary optimizer
+          #
+          # @return [undefined]
+          #
+          # @api private
           def initialize(*)
             super
             @left  = optimize_left
@@ -13,54 +33,109 @@ module Veritas
 
         private
 
+          # Optimize the left operand
+          #
+          # @return [Relation]
+          #
+          # @api private
           def optimize_left
             operation.left.optimize
           end
 
+          # Optimize the right operand
+          #
+          # @return [Relation]
+          #
+          # @api private
           def optimize_right
             operation.right.optimize
           end
 
+          # Optimize when operands are equal
           module EqualOperands
+
+            # Test if the operands are equal
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left.eql?(right)
             end
 
           end # module EqualOperands
 
+          # Optimize when the left operand is Empty
           module EmptyLeft
+
+            # Test if the left operand is empty
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left.kind_of?(Veritas::Relation::Empty)
             end
 
           end # module EmptyLeft
 
+          # Optimize when the right operand is Empty
           module EmptyRight
+
+            # Test if the right operand is empty
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               right.kind_of?(Veritas::Relation::Empty)
             end
 
           end # module EmptyRight
 
+          # Optimize when the operands are Materialized
           class MaterializedOperand < self
+
+            # Test if the operands are materialized
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left.kind_of?(Veritas::Relation::Materialized) &&
               right.kind_of?(Veritas::Relation::Materialized)
             end
 
+            # Return the materialized operation
+            #
+            # @return [Relation::Materialized]
+            #
+            # @api private
             def optimize
               operation.materialize
             end
 
           end # class MaterializedOperand
 
+          # Optimize when the operands are optimizable
           class UnoptimizedOperand < self
+
+            # Test if the operands are unoptimized
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               operation = self.operation
               !left.equal?(operation.left) ||
               !right.equal?(operation.right)
             end
 
+            # Return a Relation with optimized operands
+            #
+            # @return [Relation]
+            #
+            # @api private
             def optimize
               operation.class.new(left, right)
             end
