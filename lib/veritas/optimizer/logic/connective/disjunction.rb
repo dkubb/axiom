e0cc1a9828121e75e4acc17ee2ca153afd96a3af
Dan Kubb <dan.kubb@gmail.com>
2010-11-15 21:27:20 -0800

    Added YARD docs for Optimizer::Logic::Connective::Disjunction

--- lib/veritas/optimizer/logic/connective/disjunction.rb
+++ lib/veritas/optimizer/logic/connective/disjunction.rb
@@ -2,34 +2,73 @@ module Veritas
   class Optimizer
     module Logic
       module Connective
+
+        # Abstract base class representing Disjunction optimizations
         class Disjunction < Binary
+
+          # Optimize when the left operand is false
           class FalseLeftOperand < self
+
+            # Test if the left operand is false
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left_false?
             end
 
+            # A Disjunction with a false left operand is equivalent to the right
+            #
+            # @return [Expression]
+            #
+            # @api private
             def optimize
               right
             end
 
           end # class FalseLeftOperand
 
+          # Optimize when the right operand is false
           class FalseRightOperand < self
+
+            # Test if the right operand is false
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               right_false?
             end
 
+            # A Disjunction with a false right operand is equivalent to the left
+            #
+            # @return [Expression]
+            #
+            # @api private
             def optimize
               left
             end
 
           end # class FalseRightOperand
 
+          # Optimize when the operands are equality predicates for the same attribute
           class OptimizableToInclusion < self
+
+            # Test if the operands are equality predicates for the same attribute
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               equality_with_same_attributes?
             end
 
+            # Return an Inclusion for an attribute having a set of values
+            #
+            # @return [Inclusion]
+            #
+            # @api private
             def optimize
               left = self.left
               Veritas::Logic::Predicate::Inclusion.new(left.left, [ left.right, right.right ]).optimize
@@ -37,7 +76,14 @@ module Veritas
 
           end # class OptimizableToInclusion
 
+          # Optimize when the operands are always true
           class AlwaysTrue < self
+
+            # Test if the operands are always true
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left_true?                       ||
               right_true?                      ||
@@ -45,6 +91,11 @@ module Veritas
               left.inverse.eql?(right)
             end
 
+            # Return true
+            #
+            # @return [True]
+            #
+            # @api private
             def optimize
               Veritas::Logic::Proposition::True.instance
             end
