ce6b3408539ee3613ed794fe78817a756cec17d2
Dan Kubb <dan.kubb@gmail.com>
2010-11-15 21:21:22 -0800

    Added YARD docs for Optimizer::Logic::Connective::Conjunction

--- lib/veritas/optimizer/logic/connective/conjunction.rb
+++ lib/veritas/optimizer/logic/connective/conjunction.rb
@@ -2,34 +2,73 @@ module Veritas
   class Optimizer
     module Logic
       module Connective
+
+        # Abstract base class representing Conjunction optimizations
         class Conjunction < Binary
+
+          # Optimize when the left operand is true
           class TrueLeftOperand < self
+
+            # Test if the left operand is true
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left_true?
             end
 
+            # A Conjunction with a true left operand is equivalent to the right
+            #
+            # @return [Expression]
+            #
+            # @api private
             def optimize
               right
             end
 
           end # class TrueLeftOperand
 
+          # Optimize when the right operand is true
           class TrueRightOperand < self
+
+            # Test if the right operand is true
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               right_true?
             end
 
+            # A Conjunction with a true right operand is equivalent to the left
+            #
+            # @return [Expression]
+            #
+            # @api private
             def optimize
               left
             end
 
           end # class TrueRightOperand
 
+          # Optimize when the operands are inequality predicates for the same attribute
           class OptimizableToExclusion < self
+
+            # Test if the operands are inequality predicates for the same attribute
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               inequality_with_same_attributes?
             end
 
+            # Return an Exclusion for an attribute against a set of values
+            #
+            # @return [Exclusion]
+            #
+            # @api private
             def optimize
               left = self.left
               Veritas::Logic::Predicate::Exclusion.new(left.left, [ left.right, right.right ]).optimize
@@ -37,7 +76,14 @@ module Veritas
 
           end # class OptimizableToExclusion
 
+          # Optimize when the operands are always false
           class AlwaysFalse < self
+
+            # Test if the operands are always false
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left_false?                    ||
               right_false?                   ||
@@ -45,6 +91,11 @@ module Veritas
               left.inverse.eql?(right)
             end
 
+            # Return false
+            #
+            # @return [False]
+            #
+            # @api private
             def optimize
               Veritas::Logic::Proposition::False.instance
             end
