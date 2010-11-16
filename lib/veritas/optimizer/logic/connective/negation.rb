0b4709346c74d6797c9dc79eb32d1d0dc43e7771
Dan Kubb <dan.kubb@gmail.com>
2010-11-15 21:30:23 -0800

    Added YARD docs for Optimizer::Logic::Connective::Negation

--- lib/veritas/optimizer/logic/connective/negation.rb
+++ lib/veritas/optimizer/logic/connective/negation.rb
@@ -2,9 +2,22 @@ module Veritas
   class Optimizer
     module Logic
       module Connective
+
+        # Abstract base class representing Negation optimizations
         class Negation < Optimizer
+
+          # The optimized operand
+          #
+          # @return [Expression]
+          #
+          # @api private
           attr_reader :operand
 
+          # Initialize a Unary optimizer
+          #
+          # @return [undefined]
+          #
+          # @api private
           def initialize(*)
             super
             @operand = optimize_operand
@@ -12,16 +25,33 @@ module Veritas
 
         private
 
+          # Optimize the operand
+          #
+          # @return [Expression]
+          #
+          # @api private
           def optimize_operand
             # TODO: move Predicate.optimize_operand to a better location
             Predicate.optimize_operand(operation.operand)
           end
 
+          # Optimzie when the operand can be inverted
           class InvertibleOperand < self
+
+            # Test if the operand can be inverted
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               operand.respond_to?(:inverse)
             end
 
+            # A Negation of an Expression is equivalent to the inverted Expression
+            #
+            # @return [Expression]
+            #
+            # @api private
             def optimize
               operand.inverse
             end
