f6c1da40574be5c92ab8d4aa785fba10963de184
Dan Kubb <dan.kubb@gmail.com>
2010-11-16 20:48:19 -0800

    Added YARD docs for Optimizer::Logic::Predicate::LessThan

--- lib/veritas/optimizer/logic/predicate/less_than.rb
+++ lib/veritas/optimizer/logic/predicate/less_than.rb
@@ -2,12 +2,20 @@ module Veritas
   class Optimizer
     module Logic
       class Predicate
+
+        # Abstract base class representing LessThan optimizations
         class LessThan < self
           include Comparable
 
+          # Optimize when the operands are always false
           class AlwaysFalse < self
             include Predicate::AlwaysFalse
 
+            # Test if the operands are always false
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left.equal?(right) ||
               LessThanOrEqualTo::AlwaysFalse.new(operation).optimizable?
@@ -15,9 +23,15 @@ module Veritas
 
           end # class AlwaysFalse
 
+          # Optimize when the operands are always true
           class AlwaysTrue < self
             include Predicate::AlwaysTrue
 
+            # Test if the operands are always true
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               operation.class.call(Predicate::Util.max(left), Predicate::Util.min(right))
             end
