10c24e3ee70c966f8e8ecd2b85a7bbd3c88fca70
Dan Kubb <dan.kubb@gmail.com>
2010-11-16 20:45:39 -0800

    Added YARD docs for Optimizer::Logic::Predicate::GreaterThan

--- lib/veritas/optimizer/logic/predicate/greater_than.rb
+++ lib/veritas/optimizer/logic/predicate/greater_than.rb
@@ -2,12 +2,20 @@ module Veritas
   class Optimizer
     module Logic
       class Predicate
+
+        # Abstract base class representing GreaterThan optimizations
         class GreaterThan < self
           include Comparable
 
+          # Optimize when the operands are always false
           class AlwaysFalse < self
             include Predicate::AlwaysFalse
 
+            # Test if the operands are always false
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left.equal?(right) ||
               GreaterThanOrEqualTo::AlwaysFalse.new(operation).optimizable?
@@ -15,9 +23,15 @@ module Veritas
 
           end # class AlwaysFalse
 
+          # Optimize when the operands are always true
           class AlwaysTrue < self
             include Predicate::AlwaysTrue
 
+            # Test if the operands are always true
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               operation.class.call(Predicate::Util.min(left), Predicate::Util.max(right))
             end
