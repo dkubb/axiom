75f49cc50f7d80f2d83e813ac500ff4a4c627bcd
Dan Kubb <dan.kubb@gmail.com>
2010-11-16 20:38:30 -0800

    Added YARD docs for Optimizer::Logic::Predicate::Equality

--- lib/veritas/optimizer/logic/predicate/equality.rb
+++ lib/veritas/optimizer/logic/predicate/equality.rb
@@ -2,17 +2,26 @@ module Veritas
   class Optimizer
     module Logic
       class Predicate
+
+        # Abstract base class representing Equality optimizations
         class Equality < self
           include Comparable
 
+          # Optimize when the operand are always false
           class AlwaysFalse < self
             include Comparable::NeverEquivalent
             include Predicate::AlwaysFalse
           end # class AlwaysFalse
 
+          # Optimize when the operand are always true
           class AlwaysTrue < self
             include Predicate::AlwaysTrue
 
+            # Test if the operands are always true
+            #
+            # @return [Boolean]
+            #
+            # @api private
             def optimizable?
               left.equal?(right)
             end
