180699efc80f625797be88c1fb71077a7cb3e66d
Dan Kubb <dan.kubb@gmail.com>
2010-11-16 21:07:18 -0800

    Added class descriptions for Match and NoMatch optimizers

--- lib/veritas/optimizer/logic/predicate/match.rb
+++ lib/veritas/optimizer/logic/predicate/match.rb
@@ -2,6 +2,8 @@ module Veritas
   class Optimizer
     module Logic
       class Predicate
+
+        # Abstract base class representing Match optimizations
         class Match < self
 
           Veritas::Logic::Predicate::Match.optimizer = chain(
--- lib/veritas/optimizer/logic/predicate/no_match.rb
+++ lib/veritas/optimizer/logic/predicate/no_match.rb
@@ -2,6 +2,8 @@ module Veritas
   class Optimizer
     module Logic
       class Predicate
+
+        # Abstract base class representing NoMatch optimizations
         class NoMatch < self
 
           Veritas::Logic::Predicate::NoMatch.optimizer = chain(
