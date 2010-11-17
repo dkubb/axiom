25436ddfda6839b9c1797cdc663c7e7ec53b1ea2
Dan Kubb <dan.kubb@gmail.com>
2010-11-16 20:43:59 -0800

    Added YARD docs for Optimizer::Logic::Predicate::Inclusion

--- lib/veritas/optimizer/logic/predicate/inclusion.rb
+++ lib/veritas/optimizer/logic/predicate/inclusion.rb
@@ -2,21 +2,35 @@ module Veritas
   class Optimizer
     module Logic
       class Predicate
+
+        # Abstract base class representing Inclusion optimizations
         class Inclusion < self
           include Enumerable
 
+          # Optimize when the right operand is empty
           class EmptyRightOperand < self
             include Enumerable::EmptyRightOperand
 
+            # An Inclusion with an empty right operand matches nothing
+            #
+            # @return [False]
+            #
+            # @api private
             def optimize
               Veritas::Logic::Proposition::False.instance
             end
 
           end # class EmptyRightOperand
 
+          # Optimize when the right operand has one entry
           class OneRightOperand < self
             include Enumerable::OneRightOperand
 
+            # An Inclusion with a single right operand is equivalent to an Equality
+            #
+            # @return [Equality]
+            #
+            # @api private
             def optimize
               Veritas::Logic::Predicate::Equality.new(left, right.first)
             end
