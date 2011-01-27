302be30ca85e2945b1a7f1abc887d8bfa49e88f3
Bernard Lambeau <blambeau@gmail.com>
2011-01-27 19:27:33 +0100

    Added a summarization spec that shows a failure

---
+++ spec/integration/veritas/algebra/summarization_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'Veritas::Algebra::Summarization' do
+  subject { relation.summarize(by) { |r| r.add(:count) { |accumulator, _| accumulator.to_i + 1 } } }
+
+  let(:header)    { [ [ :name, String ], [ :qty, Integer ] ]                       }
+  let(:relation)  { Relation.new(header, [ [ 'Dan Kubb', 1 ], [ 'John Doe', 1 ] ]) }
+  let(:by_header) { header.values_at(0)                                            }
+
+  context 'summarize on the same set' do
+    let(:by) { Relation.new(by_header, [ [ 'Dan Kubb' ], [ 'John Doe' ] ]) }
+
+    it 'returns a relation with a single tuple' do
+      should == [ [ 'Dan Kubb', 1 ], [ 'John Doe', 1 ] ]
+    end
+  end
+
+  context 'summarize on an smaller set' do
+    let(:by) { Relation.new(by_header, [ [ 'Dan Kubb' ] ]) }
+
+    it 'returns a relation with a single tuple' do
+      should == [ [ 'Dan Kubb', 1 ] ]
+    end
+  end
+
+  context 'summarize on an larger set' do
+    let(:by) { Relation.new(by_header, [ [ 'Dan Kubb' ], [ 'Dane Largy' ] ]) }
+
+    it 'returns a relation with a single tuple' do
+      pending 'TODO: fix error when Proc-based aggregate functions can have a default accumulator specified' do
+        should == [ [ 'Dan Kubb', 1 ], [ 'Dane Largy', 0 ] ]
+      end
+    end
+  end
+end
