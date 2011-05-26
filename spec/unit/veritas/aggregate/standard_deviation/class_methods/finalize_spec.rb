933914478d6b4c51334359e8ed02837d8ac0dd49
Dan Kubb <dan.kubb@gmail.com>
2011-05-26 09:39:52 -0700

    Added spec to make sure StandardDeviation#finalize can handle a variance of 0

--- spec/unit/veritas/aggregate/standard_deviation/class_methods/finalize_spec.rb
+++ spec/unit/veritas/aggregate/standard_deviation/class_methods/finalize_spec.rb
@@ -5,11 +5,22 @@ require 'spec_helper'
 describe Aggregate::StandardDeviation, '.finalize' do
   subject { object.finalize(accumulator) }
 
-  let(:object)         { described_class                 }
-  let(:accumulator)    { [ count, mean, sum_of_squares ] }
-  let(:count)          { 6                               }
-  let(:mean)           { 3.5                             }
-  let(:sum_of_squares) { 17.5                            }
+  let(:object)      { described_class                 }
+  let(:accumulator) { [ count, mean, sum_of_squares ] }
 
-  it { should be_close(1.70, 0.01) }
+  context 'when the variance is 0.0' do
+    let(:count)          { 0   }
+    let(:mean)           { nil }
+    let(:sum_of_squares) { 0.0 }
+
+    it { should eql(0.0) }
+  end
+
+  context 'when the variance is not 0.0' do
+    let(:count)          { 6    }
+    let(:mean)           { 3.5  }
+    let(:sum_of_squares) { 17.5 }
+
+    it { should be_close(1.70, 0.01) }
+  end
 end
