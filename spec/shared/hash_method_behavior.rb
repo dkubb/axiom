1ce0652a16932e555a79e0622b0333f4b3590797
Dan Kubb <dan.kubb@gmail.com>
2011-10-03 22:46:37 -0700

    Change specs for #hash to ensure the return value is a Fixnum

--- spec/shared/hash_method_behavior.rb
+++ spec/shared/hash_method_behavior.rb
@@ -3,7 +3,7 @@
 shared_examples_for 'a hash method' do
   it_should_behave_like 'an idempotent method'
 
-  it { should be_kind_of(Integer) }
+  it { should be_instance_of(Fixnum) }
 
   it 'memoizes the hash code' do
     subject.should eql(object.memoized(:hash))
