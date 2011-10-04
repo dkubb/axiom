e3e4efa00dbebc36f97040385bb37b9d9dc9b4fd
Dan Kubb <dan.kubb@gmail.com>
2011-10-03 23:40:30 -0700

    Change shared spec to expect different behaviour for #hash under rubinius
    * Need to follow up with rbx team to see if #hash has the same behaviour
      as under mri/jruby, where it will always return a Fixnum.

--- spec/shared/hash_method_behavior.rb
+++ spec/shared/hash_method_behavior.rb
@@ -3,7 +3,12 @@
 shared_examples_for 'a hash method' do
   it_should_behave_like 'an idempotent method'
 
-  it { should be_instance_of(Fixnum) }
+  # TOOD: figure out if #hash under rbx should also always use Fixnum
+  if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+    it { should be_kind_of(Integer) }
+  else
+    it { should be_instance_of(Fixnum) }
+  end
 
   it 'memoizes the hash code' do
     subject.should eql(object.memoized(:hash))
