ba68e6fb169663d726145110634a8c32dd285858
Dan Kubb <dan.kubb@gmail.com>
2011-10-06 23:14:12 -0700

    Fix spec failures under MRI by not using lambda when I don't care about the args

--- spec/shared/hash_method_behavior.rb
+++ spec/shared/hash_method_behavior.rb
@@ -3,7 +3,7 @@
 shared_examples_for 'a hash method' do
   it_should_behave_like 'an idempotent method'
 
-  specification = lambda do
+  specification = proc do
     should be_instance_of(Fixnum)
   end
 
