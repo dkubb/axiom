a6eb2162fb86858eb1af019be2ba60e186f93a19
Dan Kubb <dan.kubb@gmail.com>
2012-02-27 23:21:35 -0800

    Fix indentation in spec

--- spec/unit/veritas/relation/operation/binary/class_methods/new_spec.rb
+++ spec/unit/veritas/relation/operation/binary/class_methods/new_spec.rb
@@ -3,38 +3,38 @@
 require 'spec_helper'
 require File.expand_path('../../fixtures/classes', __FILE__)
 
- context 'Veritas::Relation::Operation::Binary::ClassMethods#new' do
-   subject { object.new(left, right) }
+context 'Veritas::Relation::Operation::Binary::ClassMethods#new' do
+  subject { object.new(left, right) }
 
-   let(:original_left)  { Relation.new([ [ :id,   Integer ] ], [ [ 1 ] ])          }
-   let(:original_right) { Relation.new([ [ :name, String  ] ], [ [ 'Dan Kubb' ] ]) }
-   let(:object)         { BinaryRelationOperationSpecs::Object                     }
+  let(:original_left)  { Relation.new([ [ :id,   Integer ] ], [ [ 1 ] ])          }
+  let(:original_right) { Relation.new([ [ :name, String  ] ], [ [ 'Dan Kubb' ] ]) }
+  let(:object)         { BinaryRelationOperationSpecs::Object                     }
 
-   context 'with left and right ordered' do
-     let(:left)  { original_left.sort_by  { |r| r.id   } }
-     let(:right) { original_right.sort_by { |r| r.name } }
+  context 'with left and right ordered' do
+    let(:left)  { original_left.sort_by  { |r| r.id   } }
+    let(:right) { original_right.sort_by { |r| r.name } }
 
-     it { should be_instance_of(object) }
-   end
+    it { should be_instance_of(object) }
+  end
 
-   context 'with left and right unordered' do
-     let(:left)  { original_left  }
-     let(:right) { original_right }
+  context 'with left and right unordered' do
+    let(:left)  { original_left  }
+    let(:right) { original_right }
 
-     it { should be_instance_of(object) }
-   end
+    it { should be_instance_of(object) }
+  end
 
-   context 'with left ordered, and right unordered' do
-     let(:left)  { original_left.sort_by { |r| r.id } }
-     let(:right) { original_right                     }
+  context 'with left ordered, and right unordered' do
+    let(:left)  { original_left.sort_by { |r| r.id } }
+    let(:right) { original_right                     }
 
-     specify { expect { subject }.to raise_error(RelationMismatchError, 'both relations must be ordered or neither may be ordered') }
-   end
+    specify { expect { subject }.to raise_error(RelationMismatchError, 'both relations must be ordered or neither may be ordered') }
+  end
 
-   context 'with right ordered, and left unordered' do
-     let(:left)  { original_left                         }
-     let(:right) { original_right.sort_by { |r| r.name } }
+  context 'with right ordered, and left unordered' do
+    let(:left)  { original_left                         }
+    let(:right) { original_right.sort_by { |r| r.name } }
 
-     specify { expect { subject }.to raise_error(RelationMismatchError, 'both relations must be ordered or neither may be ordered') }
-   end
+    specify { expect { subject }.to raise_error(RelationMismatchError, 'both relations must be ordered or neither may be ordered') }
+  end
 end
