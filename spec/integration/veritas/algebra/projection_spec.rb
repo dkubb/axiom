402893a609ecac15c1350a2a9e39007a033f9a2a
Dan Kubb <dan.kubb@gmail.com>
2012-03-02 09:12:56 -0800

    Change spec to initialize a smaller set of integers

--- spec/integration/veritas/algebra/projection_spec.rb
+++ spec/integration/veritas/algebra/projection_spec.rb
@@ -6,7 +6,7 @@ describe Algebra::Projection do
   context 'remove attributes in predicate' do
     subject { restriction.project([ :name ]) }
 
-    let(:left)        { Relation.new([ [ :id, Integer ] ], (1..100).map { |n| [ n ] })                          }
+    let(:left)        { Relation.new([ [ :id, Integer ] ], (0..11).map { |n| [ n ] })                           }
     let(:right)       { Relation.new([ [ :name, String ] ], [ [ 'Dan Kubb' ], [ 'John Doe' ], [ 'Jane Doe' ] ]) }
     let(:relation)    { left * right                                                                            }
     let(:restriction) { relation.restrict { |r| r.id.gte(1).and(r.id.lte(10)).and(r.name.eq('Dan Kubb')) }      }
