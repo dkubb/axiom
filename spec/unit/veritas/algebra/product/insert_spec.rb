dc33fa579f0b305e56be5b123fba2b1cecd7a3fb
Dan Kubb <dan.kubb@gmail.com>
2012-05-30 21:10:58 -0700

    Fix spec formatting

--- spec/unit/veritas/algebra/product/insert_spec.rb
+++ spec/unit/veritas/algebra/product/insert_spec.rb
@@ -5,10 +5,10 @@ require 'spec_helper'
 describe Algebra::Product, '#insert' do
   subject { object.insert(other) }
 
-  let(:object)  { described_class.new(left, right)                        }
-  let(:other)   { stub('other')                                           }
-  let(:left)    { Relation.new([ [ :id, Integer ] ], [ [ 1 ] ])           }
-  let(:right)   { Relation.new([ [ :name, String ] ], [ [ 'John Doe' ] ]) }
+  let(:object) { described_class.new(left, right)                        }
+  let(:other)  { stub('other')                                           }
+  let(:left)   { Relation.new([ [ :id, Integer ] ], [ [ 1 ] ])           }
+  let(:right)  { Relation.new([ [ :name, String ] ], [ [ 'John Doe' ] ]) }
 
   specify { expect { subject }.to raise_error(ImmutableRelationError, 'inserting into a product is impossible') }
 end
