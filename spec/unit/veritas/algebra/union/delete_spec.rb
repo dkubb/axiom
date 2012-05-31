88a26872848625dbbdd1d4122796bd4e0e2a23c6
Dan Kubb <dan.kubb@gmail.com>
2012-05-31 13:56:10 -0700

    Update specs for Union#insert and #delete to ensure the other relation is coerced
    * Since these two methods delegate to other relations' #insert and #delete
      method, coercion happens automatically. These specs are only added to ensure
      that tbe behaviour remains consistent.

--- spec/unit/veritas/algebra/union/delete_spec.rb
+++ spec/unit/veritas/algebra/union/delete_spec.rb
@@ -5,19 +5,40 @@ require 'spec_helper'
 describe Algebra::Union, '#delete' do
   subject { object.delete(other) }
 
-  let(:object) { described_class.new(left, right)                     }
-  let(:left)   { Relation.new([ [ :id, Integer ] ], [ [ 1 ], [ 2 ] ]) }
-  let(:right)  { Relation.new([ [ :id, Integer ] ], [ [ 2 ], [ 3 ] ]) }
-  let(:other)  { Relation.new([ [ :id, Integer ] ], [ [ 2 ] ])        }
+  let(:object)         { described_class.new(left, right)       }
+  let(:left)           { Relation.new(header, [ [ 1 ], [ 2 ] ]) }
+  let(:right)          { Relation.new(header, [ [ 2 ], [ 3 ] ]) }
+  let(:other_relation) { Relation.new(header, [ [ 2 ] ])        }
+  let(:header)         { [ [ :id, Integer ] ]                   }
 
-  it { should be_instance_of(described_class) }
+  shared_examples_for 'Algebra::Union#delete' do
+    it { should be_instance_of(described_class) }
 
-  its(:left)  { should eql(left.delete(other))  }
-  its(:right) { should eql(right.delete(other)) }
+    its(:left)  { should eql(left.delete(other))  }
+    its(:right) { should eql(right.delete(other)) }
 
-  its(:header) { should == [ [ :id, Integer ] ] }
+    its(:header) { should == header }
 
-  it 'deletes the tuples' do
-    should == [ [ 1 ], [ 3 ] ]
+    it 'deletes the tuples' do
+      should == [ [ 1 ], [ 3 ] ]
+    end
+  end
+
+  context 'with a relation' do
+    let(:other) { other_relation }
+
+    it_should_behave_like 'Algebra::Union#delete'
+  end
+
+  context 'with an array of tuples' do
+    let(:other) { other_relation.to_a }
+
+    it_should_behave_like 'Algebra::Union#delete'
+  end
+
+  context 'with an array of arrays' do
+    let(:other) { other_relation.map(&:to_ary) }
+
+    it_should_behave_like 'Algebra::Union#delete'
   end
 end
--- spec/unit/veritas/algebra/union/insert_spec.rb
+++ spec/unit/veritas/algebra/union/insert_spec.rb
@@ -5,19 +5,40 @@ require 'spec_helper'
 describe Algebra::Union, '#insert' do
   subject { object.insert(other) }
 
-  let(:object) { described_class.new(left, right)              }
-  let(:left)   { Relation.new([ [ :id, Integer ] ], [ [ 1 ] ]) }
-  let(:right)  { Relation.new([ [ :id, Integer ] ], [ [ 2 ] ]) }
-  let(:other)  { Relation.new([ [ :id, Integer ] ], [ [ 3 ] ]) }
+  let(:object)         { described_class.new(left, right) }
+  let(:left)           { Relation.new(header, [ [ 1 ] ])  }
+  let(:right)          { Relation.new(header, [ [ 2 ] ])  }
+  let(:other_relation) { Relation.new(header, [ [ 3 ] ])  }
+  let(:header)         { [ [ :id, Integer ] ]             }
 
-  it { should be_instance_of(described_class) }
+  shared_examples_for 'Algebra::Union#insert' do
+    it { should be_instance_of(described_class) }
 
-  its(:left)  { should eql(left.insert(other))  }
-  its(:right) { should eql(right.insert(other)) }
+    its(:left)  { should eql(left.insert(other))  }
+    its(:right) { should eql(right.insert(other)) }
 
-  its(:header) { should == [ [ :id, Integer ] ] }
+    its(:header) { should == header }
 
-  it 'inserts the tuples' do
-    should == [ [ 1 ], [ 2 ], [ 3 ] ]
+    it 'inserts the tuples' do
+      should == [ [ 1 ], [ 2 ], [ 3 ] ]
+    end
+  end
+
+  context 'with a relation' do
+    let(:other) { other_relation }
+
+    it_should_behave_like 'Algebra::Union#insert'
+  end
+
+  context 'with an array of tuples' do
+    let(:other) { other_relation.to_a }
+
+    it_should_behave_like 'Algebra::Union#insert'
+  end
+
+  context 'with an array of arrays' do
+    let(:other) { other_relation.map(&:to_ary) }
+
+    it_should_behave_like 'Algebra::Union#insert'
   end
 end
