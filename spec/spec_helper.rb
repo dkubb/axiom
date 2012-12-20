b7594964c8b2f4fbb97cddec528a4e37159c890d
Dan Kubb <dan.kubb@gmail.com>
2012-12-20 15:06:49 -0800

    Fix code to use better variable names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,9 @@ require 'spec/autorun'
 include Veritas
 
 # require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
+  require file
+end
 
 Spec::Runner.configure do |config|
 
