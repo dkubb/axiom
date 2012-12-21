da91e6e3afdbe391df22b9ce3265c23960a2e434
Dan Kubb <dan.kubb@gmail.com>
2012-12-20 23:03:34 -0800

    Fix specs to run under rbx 2.0.0-rc1 in 1.8 mode

--- spec/support/lazy_enumerable.rb
+++ spec/support/lazy_enumerable.rb
@@ -2,6 +2,18 @@
 
 require 'delegate'
 
+# This is a work-around for a bug in rubinius 2.0.0-rc1
+# See: https://github.com/rubinius/rubinius/issues/2104
+SimpleDelegator.class_eval do
+  if instance_method(:dup).arity == 1
+    def dup
+      new = super
+      new.__setobj__(__getobj__.dup)
+      new
+    end
+  end
+end
+
 class LazyEnumerable < SimpleDelegator
   def size
     nil
