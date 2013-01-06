5b433e9eeaadb68b7572f17b50a4db648f544671
Dan Kubb <dan.kubb@gmail.com>
2013-01-05 23:23:42 -0800

    Add spec for Relation::Operation::Insertion#initialize

---
+++ spec/unit/veritas/relation/operation/insertion/initialize_spec.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Relation::Operation::Insertion, '#initialize' do
+  subject { described_class.new(base, other) }
+
+  let(:base)  { Relation.new([ [ :id, Integer ] ], [ [ 1 ] ]) }
+  let(:other) { Relation.new([ [ :id, Integer ] ], [ [ 2 ] ]) }
+
+  it { should be_instance_of(described_class) }
+
+  its(:left) { should be(base) }
+
+  its(:right) { should be(other) }
+end
