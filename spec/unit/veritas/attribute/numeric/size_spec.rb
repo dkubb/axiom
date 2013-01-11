b8087f43b60ec6adc2207366606e6324a5b6d4fa
Dan Kubb <dan.kubb@gmail.com>
2013-01-11 12:08:38 -0800

    Fix Attribute::Numeric#size spec to kill a mutation

--- spec/unit/veritas/attribute/numeric/size_spec.rb
+++ spec/unit/veritas/attribute/numeric/size_spec.rb
@@ -9,11 +9,23 @@ require 'spec_helper'
     context 'without :size option passed to constructor' do
       let(:object) { described_class.new(:id) }
 
+      it_should_behave_like 'an idempotent method'
+
       it { should eql(-Float::INFINITY..Float::INFINITY) }
     end
 
-    context 'with :size option passed to constructor' do
+    context 'with a :size option passed to constructor that is inclusive' do
       let(:object) { described_class.new(:id, :size => 1..100) }
+
+      it_should_behave_like 'an idempotent method'
+
+      it { should eql(1..100) }
+    end
+
+    context 'with a :size option passed to constructor that is exclusive' do
+      let(:object) { described_class.new(:id, :size => 1...101) }
+
+      it_should_behave_like 'an idempotent method'
 
       it { should eql(1..100) }
     end
