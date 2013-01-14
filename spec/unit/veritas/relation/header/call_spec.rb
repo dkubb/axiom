640e4e40a2325f7beb51bdada885ec7db4eae045
Dan Kubb <dan.kubb@gmail.com>
2013-01-13 22:18:42 -0800

    Fix Header#call spec to kill mutation

--- spec/unit/veritas/relation/header/call_spec.rb
+++ spec/unit/veritas/relation/header/call_spec.rb
@@ -6,12 +6,25 @@ require 'spec_helper'
   describe Relation::Header, "##{method}" do
     subject { object.send(method, name) }
 
-    let(:object) { described_class.coerce([ [ :id, Integer ] ]) }
+    let(:object)    { described_class.coerce([ attribute ]) }
+    let(:attribute) { Attribute::Integer.new(:id)           }
+
+    context 'with a known attribute' do
+      let(:name) { attribute }
+
+      it { should == [ :id, Integer ] }
+    end
 
     context 'with a known attribute name' do
       let(:name) { :id }
 
       it { should == [ :id, Integer ] }
+    end
+
+    context 'with an unknown attribute' do
+      let(:name) { Attribute::String.new(:name) }
+
+      specify { expect { subject }.to raise_error(UnknownAttributeError, 'the attribute name is unknown') }
     end
 
     context 'with an unknown attribute name' do
