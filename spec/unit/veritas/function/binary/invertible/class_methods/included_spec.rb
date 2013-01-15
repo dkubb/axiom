1f8fc2183f369ff98ad0e6561013116d99467e7b
Dan Kubb <dan.kubb@gmail.com>
2013-01-14 23:05:45 -0800

    Add spec for Function::Binary::Invertible.included

---
+++ spec/unit/veritas/function/binary/invertible/class_methods/included_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Function::Binary::Invertible, '.included' do
+  subject { object }
+
+  let(:object) { described_class }
+  let(:klass)  { Class.new       }
+
+  before do
+    klass.stub(:memoize)
+  end
+
+  it 'memoizes the inverse method' do
+    klass.should_receive(:memoize).with(:inverse)
+    klass.send(:include, subject)
+  end
+
+  it 'delegates to the ancestor' do
+    included_ancestor = false
+    subject.extend Module.new {
+      define_method(:included) { |_| included_ancestor = true }
+    }
+    expect {
+      klass.send(:include, subject)
+    }.to change { included_ancestor }.from(false).to(true)
+  end
+end
