a19d31d4cdec405c7047339e294e592c82a836bd
Dan Kubb <dan.kubb@gmail.com>
2013-07-31 12:48:21 -0700

    Updated simplecov configuration to exclude vendor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,12 @@ if ENV['COVERAGE'] == 'true'
  ]
 
   SimpleCov.start do
-    command_name     'spec:unit'
-    add_filter       'config'
-    add_filter       'spec'
+    command_name 'spec:unit'
+
+    add_filter 'config'
+    add_filter 'spec'
+    add_filter 'vendor'
+
     minimum_coverage 100
   end
 end
