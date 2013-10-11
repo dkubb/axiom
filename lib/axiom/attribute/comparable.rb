e33d1e8071bd2db70cc85382f8db5855898da9bc
Dan Kubb <dan.kubb@gmail.com>
2013-10-10 22:08:36 -0700

    Change code to qualify constants to avoid ambiguity in the future

--- lib/axiom/attribute/comparable.rb
+++ lib/axiom/attribute/comparable.rb
@@ -20,11 +20,11 @@ module Axiom
       # @example
       #   ascending = attribute.asc
       #
-      # @return [Relation::Operation::Order::Ascending]
+      # @return [Axiom::Relation::Operation::Order::Ascending]
       #
       # @api public
       def asc
-        Relation::Operation::Order::Ascending.new(self)
+        Axiom::Relation::Operation::Order::Ascending.new(self)
       end
 
       # Sort the attribute in descending order
@@ -32,11 +32,11 @@ module Axiom
       # @example
       #   descending = attribute.desc
       #
-      # @return [Relation::Operation::Order::Descending]
+      # @return [Axiom::Relation::Operation::Order::Descending]
       #
       # @api public
       def desc
-        Relation::Operation::Order::Descending.new(self)
+        Axiom::Relation::Operation::Order::Descending.new(self)
       end
 
     end # module Comparable
