d4d04a81b7c66649f35db38b88eafaf111e207d0
Dan Kubb <dan.kubb@gmail.com>
2013-10-27 23:23:12 -0700

    Fix mutations in Projection#assert_removed_attributes_optional

--- lib/axiom/algebra/projection.rb
+++ lib/axiom/algebra/projection.rb
@@ -102,7 +102,7 @@ module Axiom
       def assert_removed_attributes_optional
         names = required_attribute_names
         if names.any?
-          fail RequiredAttributesError, "required attributes #{names.inspect} have been removed"
+          fail RequiredAttributesError, "required attributes #{names} have been removed"
         end
       end
 
