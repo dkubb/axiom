81035d4a1a1a1b9d3c137cebb8bf43ad698c6e2d
Dan Kubb <dan.kubb@gmail.com>
2013-11-03 05:55:39 -0800

    Refactor inheritable_alias to test one behaviour each

--- spec/unit/axiom/aliasable/fixtures/classes.rb
+++ spec/unit/axiom/aliasable/fixtures/classes.rb
@@ -4,14 +4,8 @@ module AliasableSpecs
   class Object
     extend Aliasable
 
-    def test; end
-
-    def test_with_arguments(a, b)
-      [a, b]
+    def test(arguments, &block)
     end
 
-    def test_with_block(&block)
-      block
-    end
   end # class Object
 end # module AliasableSpecs
--- spec/unit/axiom/aliasable/inheritable_alias_spec.rb
+++ spec/unit/axiom/aliasable/inheritable_alias_spec.rb
@@ -6,69 +6,36 @@ require File.expand_path('../fixtures/classes', __FILE__)
 describe Aliasable, '#inheritable_alias' do
   subject { object.inheritable_alias(aliases) }
 
-  let(:aliases) do
-    {
-      other: :test,
-      other_with_arguments: :test_with_arguments,
-      other_with_block:     :test_with_block
-    }
-  end
-
   let(:object)    { Class.new(AliasableSpecs::Object) }
+  let(:aliases)   { { other: :test }                  }
   let(:aliasable) { object.new                        }
+  let(:arg)       { double('arg')                     }
 
   it_should_behave_like 'a command method'
 
-  it 'creates a method #other' do
-    expect { subject }.to change { aliasable.respond_to?(:other) }
-      .from(false)
-      .to(true)
+  it 'creates a method alias' do
+    expect { subject }
+      .to change { aliasable.respond_to?(:other) }.from(false).to(true)
   end
 
-  it 'aliases #other to #test' do
+  it 'forwards arguments to aliased method' do
     subject
-    retval = double('Return Value')
+    expect(aliasable).to receive(:test).with(arg)
+    aliasable.other(arg)
+  end
+
+  it 'forwards the block to aliased method' do
+    subject
+    yieldparam = double('yieldparam')
+    expect(aliasable).to receive(:test).and_yield(yieldparam)
+    expect { |block| aliasable.other(arg, &block) }
+      .to yield_with_args(yieldparam)
+  end
+
+  it 'returns value from aliased method' do
+    subject
+    retval = double('retval')
     expect(aliasable).to receive(:test).and_return(retval)
-    expect(aliasable.other).to be(retval)
-  end
-
-  specification = proc do
-    object.class_eval do
-      def test
-        caller
-      end
-    end
-
-    subject
-
-    file, line = aliasable.other.first.split(':')[0, 2]
-
-    expect(File.expand_path(file)).to eql(File.expand_path('../../../../../lib/axiom/support/aliasable.rb', __FILE__))
-
-    # Multiple numbers depending on context:
-    #
-    # 37 for "normal"
-    # 39 for "mutation generated code"
-    #
-    expect([37, 39]).to include(line.to_i)
-  end
-
-  it 'sets the file and line number properly' do
-    if RUBY_PLATFORM.include?('java')
-      pending('Kernel#caller returns the incorrect line number in JRuby', &specification)
-    else
-      instance_eval(&specification)
-    end
-  end
-
-  it 'forwards arguments' do
-    subject
-    expect(aliasable.other_with_arguments(:foo, :bar)).to eql([:foo, :bar])
-  end
-
-  it 'forwards block' do
-    subject
-    block = proc { }
-    expect(aliasable.other_with_block(&block)).to eql(block)
+    expect(aliasable.other(arg)).to be(retval)
   end
 end
